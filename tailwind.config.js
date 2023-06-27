037ac2f1d4c72fa8be07c714a234731df0f75d9e
Dillion Verma <hello@dillionverma.com>
2023-06-27 18:12:57 -0400

    🐛 fix: update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,6 +53,11 @@ module.exports = {
         md: `calc(var(--radius) - 2px)`,
         sm: "calc(var(--radius) - 4px)",
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        gradient: "gradient 8s linear infinite",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: 0 },
@@ -62,10 +67,11 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        gradient: {
+          to: {
+            backgroundPosition: "var(--bg-size) 0",
+          },
+        },
       },
     },
   },
