7ee6f305cf686c158207e14f0c4639fed6c0dee8
Dillion Verma <hello@dillionverma.com>
2023-07-13 13:41:27 -0700

    🐛 fix: update gradient card demo

--- registry/components/example/magic-card-demo.tsx
+++ registry/components/example/magic-card-demo.tsx
@@ -14,13 +14,13 @@ export default function MagicCardWithGradient() {
         <p className="text-4xl font-medium whitespace-nowrap text-gray-800 dark:text-gray-200 z-10">
           Magic
         </p>
-        <div className="pointer-events-none h-full absolute inset-0 bg-[radial-gradient(ellipse_80%_50%_at_50%_100%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+        <div className="pointer-events-none h-full absolute inset-0 bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
       </MagicCard>
       <MagicCard className="cursor-pointer shadow-2xl flex justify-center items-center overflow-hidden bg-background">
         <p className="text-4xl font-medium whitespace-nowrap text-gray-800 dark:text-gray-200 z-10">
           Card
         </p>
-        <div className="pointer-events-none h-full absolute inset-0 bg-[radial-gradient(ellipse_80%_50%_at_50%_100%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+        <div className="pointer-events-none h-full absolute inset-0 bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
       </MagicCard>
     </MagicContainer>
   );
