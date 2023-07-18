31bc175ef0b1088313e615ee8498ce6c869a15b6
Dillion Verma <hello@dillionverma.com>
2023-07-17 20:14:02 -0700

    ✨ feat: Setup queued scheduling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,5 @@
 const colors = require("tailwindcss/colors");
 import { type Config } from "tailwindcss";
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
-const svgToDataUri = require("mini-svg-data-uri");
 
 /** @type {import('tailwindcss').Config} */
 
@@ -100,20 +96,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require("@tailwindcss/typography"),
-    function ({ matchUtilities, theme }: { matchUtilities: any; theme: any }) {
-      matchUtilities(
-        {
-          "bg-grid": (value: string) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      );
-    },
-  ],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
