9927d78a6b928006483b8f98b3326837437372a1
Dillion Verma <hello@dillionverma.com>
2023-07-17 20:07:14 -0700

    ✨ feat: Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { allComponents, allPages } from "@/.contentlayer/generated";
+import { MetadataRoute } from "next";
+import { headers } from "next/headers";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const headersList = headers();
+  let domain = headersList.get("host") as string;
+  let protocol = "https";
+
+  return [
+    {
+      url: `${protocol}://${domain}`,
+      lastModified: new Date(),
+    },
+    ...allPages.map((post) => ({
+      url: `${protocol}://${domain}/${post.slugAsParams}`,
+      lastModified: new Date(),
+    })),
+    ...allComponents.map((post) => ({
+      url: `${protocol}://${domain}/components/${post.slugAsParams}`,
+      lastModified: post.date,
+    })),
+  ];
+}
