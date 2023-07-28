fbe3391f7bb9090546a838e6357238e6fcc7d694
Dillion Verma <hello@dillionverma.com>
2023-07-28 15:07:40 -0700

    🐛 fix: add apple support

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -78,7 +78,7 @@ export default async function Home() {
                 Introducing Marquee
               </span>
               <ChevronRight className="ml-1 h-4 w-4 text-gray-500" />
-              <div className="block w-full h-full absolute inset-0 p-[1px] [border-radius:inherit] bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] [mask-composite:xor] animate-gradient bg-[length:var(--bg-size)_100%]" />
+              <div className="block w-full h-full absolute inset-0 p-[1px] [border-radius:inherit] bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] [mask-composite:xor] [-webkit-mask-composite:xor] animate-gradient bg-[length:var(--bg-size)_100%]" />
             </Link>
           </FadeIn>
           <FadeIn
