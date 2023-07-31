10f51b18740edc17744c4727a4a3816d1a9e8b44
Dillion Verma <hello@dillionverma.com>
2023-07-31 11:09:43 -0700

    🐛 fix: meteor styles

--- registry/components/magicui/meteors.tsx
+++ registry/components/magicui/meteors.tsx
@@ -28,7 +28,7 @@ export const Meteors = ({ number = 20 }: MeteorsProps) => {
         <span
           key={idx}
           className={clsx(
-            "pointer-events-none absolute left-1/2 top-1/2 -z-10 h-0.5 w-0.5 rotate-[215deg] animate-meteor rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
+            "pointer-events-none absolute left-1/2 top-1/2 h-0.5 w-0.5 rotate-[215deg] animate-meteor rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
           )}
           style={style}
         >
