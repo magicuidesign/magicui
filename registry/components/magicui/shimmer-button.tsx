a7ea129c9ac030ce77b6e9f635fdd4e132dca1c5
Dillion Verma <hello@dillionverma.com>
2023-08-10 16:19:50 -0700

    🐛 fix: fix shimmer style

--- registry/components/magicui/shimmer-button.tsx
+++ registry/components/magicui/shimmer-button.tsx
@@ -7,7 +7,7 @@ interface ShimmerButtonProps {
   shimmerColor?: string;
   shimmerSize?: string;
   borderRadius?: string;
-  sparkDuration?: string;
+  shimmerDuration?: string;
   background?: string;
   className?: string;
   children?: React.ReactNode;
@@ -18,8 +18,8 @@ const ShimmerButton = ({
   shadowEnabled = true,
   shimmerColor = "#ffffff",
   shimmerSize = "0.1em",
+  shimmerDuration = "1.5s",
   borderRadius = "100px",
-  sparkDuration = "1.8s",
   background = "radial-gradient(ellipse 80% 50% at 50% 120%,rgba(62, 61, 117),rgba(13, 13, 33))",
   className,
   children,
@@ -28,9 +28,10 @@ const ShimmerButton = ({
     <button
       style={
         {
+          "--spread": "90deg",
           "--shimmer-color": shimmerColor,
           "--radius": borderRadius,
-          "--spark": sparkDuration,
+          "--speed": shimmerDuration,
           "--cut": shimmerSize,
           "--bg": background,
           "--shadow": shadow,
@@ -44,10 +45,14 @@ const ShimmerButton = ({
       )}
     >
       {/* spark container */}
-      <div className="absolute inset-0 rotate-0 animate-flip overflow-hidden [border-radius:var(--radius)] [container-type:size] [mask:linear-gradient(white,transparent_50%)] ">
-        {/* spark before */}
-        <div className="absolute -z-[1] w-[200%] rotate-0 animate-rotate transition-all [aspect-ratio:1] [background:conic-gradient(from_0deg,transparent_0_340deg,var(--shimmer-color)_360deg)] [inset:0_auto_auto_50%] [transform:rotate(-90deg)]  [translate:-50%_-15%] group-hover:opacity-40" />
+      <div className="absolute inset-0  overflow-visible [container-type:size]">
+        {/* spark */}
+        <div className="absolute inset-0 h-[100cqh] animate-slide [aspect-ratio:1] [border-radius:0] [mask:none] ">
+          {/* spark before */}
+          <div className="absolute inset-[-100%] w-auto rotate-0 animate-spin [background:conic-gradient(from_calc(270deg-(var(--spread)*0.5)),transparent_0,hsl(0_0%_100%/1)_var(--spread),transparent_var(--spread))] [translate:0_0]" />
+        </div>
       </div>
+
       {/* backdrop */}
       <div className="absolute [background:var(--bg)] [border-radius:var(--radius)] [inset:var(--cut)]" />
       {/* content */}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,7 +67,9 @@ module.exports = {
         grid: "grid 15s linear infinite",
         marquee: "marquee var(--duration) linear infinite",
         flip: "flip calc(var(--spark) * 2) infinite steps(2, end)",
-        rotate: "rotate var(--spark) linear infinite both;",
+        rotate: "rotate var(--spark) linear infinite both",
+        spin: "spin calc(var(--speed) * 2) infinite linear",
+        slide: "slide var(--speed) ease-in-out infinite alternate",
       },
       keyframes: {
         "accordion-down": {
@@ -106,6 +108,25 @@ module.exports = {
         rotate: {
           to: {
             transform: "rotate(90deg)",
+          },
+        },
+        spin: {
+          "0%": {
+            rotate: "0deg",
+          },
+          "15%, 35%": {
+            rotate: "90deg",
+          },
+          "65%, 85%": {
+            rotate: "270deg",
+          },
+          "100%": {
+            rotate: "360deg",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
           },
         },
       },
