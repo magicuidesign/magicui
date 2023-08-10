7e60693ee75ce609e468c8024ae8c47436754f46
Dillion Verma <hello@dillionverma.com>
2023-08-10 16:59:56 -0700

    🐛 fix: component

--- components/component-wrapper.tsx
+++ components/component-wrapper.tsx
@@ -8,20 +8,19 @@ const ComponentWrapper = ({ className, children }: ComponentWrapperProps) => {
   return (
     <div
       className={cn(
-        "not-prose max-w-screen relative flex flex-col items-center justify-center rounded-xl border p-8 backdrop-blur-xl md:p-20",
+        "not-prose max-w-screen relative flex flex-col items-center justify-center rounded-xl border bg-background p-8 md:p-20",
         className,
       )}
     >
-      {children}
-
       <div
         className={cn(
-          `absolute inset-0 -z-10 h-full w-full [background-size:16px_16px]`,
+          `absolute inset-0 h-full w-full [background-size:16px_16px]`,
           `bg-[radial-gradient(#00000055_1px,transparent_1px)] [--mask-offset:60px] dark:bg-[radial-gradient(#ffffff22_1px,transparent_1px)]`,
           `[WebkitMask:mask:linear-gradient(to_bottom,transparent,#fff_var(--mask-offset)_calc(100%-var(--mask-offset)),transparent),linear-gradient(to_right,transparent,#fff_var(--mask-offset)_calc(100%-var(--mask-offset)),transparent)] [mask:linear-gradient(to_bottom,transparent,#fff_var(--mask-offset)_calc(100%-var(--mask-offset)),transparent),linear-gradient(to_right,transparent,#fff_var(--mask-offset)_calc(100%-var(--mask-offset)),transparent)]`,
           `[--webkit-mask-composite:source-in,xor] [mask-composite:intersect]`,
         )}
       />
+      {children}
     </div>
   );
 };
