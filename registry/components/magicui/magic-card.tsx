7b0883b7ead4983695138013a8588b385384b8a6
Dillion Verma <hello@dillionverma.com>
2023-08-10 17:13:40 -0700

    🐛 fix: card

--- registry/components/magicui/magic-card.tsx
+++ registry/components/magicui/magic-card.tsx
@@ -205,20 +205,18 @@ const MagicCard = ({
       }
     >
       {/* Border */}
-      <div className="pointer-events-none absolute inset-0 -z-[3] h-full w-full rounded-2xl bg-gray-300 bg-[radial-gradient(var(--mask-size)_circle_at_var(--mouse-x)_var(--mouse-y),#ffaa40_0,#9c40ff_50%,transparent_100%)] transition-opacity duration-500 dark:bg-gray-700 " />
-
-      {children}
+      <div className="pointer-events-none absolute inset-0 h-full w-full rounded-2xl bg-gray-300 bg-[radial-gradient(var(--mask-size)_circle_at_var(--mouse-x)_var(--mouse-y),#ffaa40_0,#9c40ff_50%,transparent_100%)] transition-opacity duration-500 dark:bg-gray-700 " />
 
       {/* Background */}
-      <div
-        className={"absolute inset-[1px] -z-[2] rounded-2xl bg-background"}
-      />
+      <div className={"absolute inset-[1px] rounded-2xl bg-background"} />
+
+      {children}
 
       {/* Spotlight */}
       {spotlight && (
         <div
           className={
-            "blur-xs pointer-events-none absolute left-0 top-0 -z-[1] h-full w-full rounded-2xl bg-[radial-gradient(var(--mask-size)_circle_at_var(--mouse-x)_var(--mouse-y),var(--spotlight-color),transparent_40%)] transition-opacity duration-500"
+            "blur-xs pointer-events-none absolute left-0 top-0 h-full w-full rounded-2xl bg-[radial-gradient(var(--mask-size)_circle_at_var(--mouse-x)_var(--mouse-y),var(--spotlight-color),transparent_40%)] transition-opacity duration-500"
           }
         />
       )}
