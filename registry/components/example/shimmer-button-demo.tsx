4b5d503dca295547f3e760f46513f230049c4bae
Dillion Verma <hello@dillionverma.com>
2023-08-11 17:04:06 -0700

    🐛 fix: update button style

--- registry/components/example/shimmer-button-demo.tsx
+++ registry/components/example/shimmer-button-demo.tsx
@@ -3,7 +3,7 @@ import ShimmerButton from "@/registry/components/magicui/shimmer-button";
 
 export default function ShimmerButtonDemo() {
   return (
-    <div className="relative flex h-full w-full items-center justify-center rounded-lg border bg-background p-20 shadow-2xl">
+    <div className="relative flex h-full w-full items-center justify-center rounded-lg border bg-background py-20 shadow-2xl">
       <ShimmerButton shadowEnabled={false}>
         <span className="whitespace-pre-wrap bg-gradient-to-b from-black from-30% to-gray-300/80 bg-clip-text text-center text-sm font-semibold leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 dark:text-transparent lg:text-2xl">
           Shimmer Button
