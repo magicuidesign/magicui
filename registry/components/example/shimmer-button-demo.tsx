ead60daac70411656f18cfb078f3efb569d06423
Dillion Verma <hello@dillionverma.com>
2023-08-11 17:01:53 -0700

    🐛 fix: update button style

--- registry/components/example/shimmer-button-demo.tsx
+++ registry/components/example/shimmer-button-demo.tsx
@@ -5,7 +5,7 @@ export default function ShimmerButtonDemo() {
   return (
     <div className="relative flex h-full w-full items-center justify-center rounded-lg border bg-background p-20 shadow-2xl">
       <ShimmerButton shadowEnabled={false}>
-        <span className="whitespace-pre-wrap bg-gradient-to-b from-black from-30% to-gray-300/80 bg-clip-text text-center text-2xl font-semibold leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 dark:text-transparent">
+        <span className="whitespace-pre-wrap bg-gradient-to-b from-black from-30% to-gray-300/80 bg-clip-text text-center text-sm font-semibold leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 dark:text-transparent lg:text-2xl">
           Shimmer Button
         </span>
       </ShimmerButton>
