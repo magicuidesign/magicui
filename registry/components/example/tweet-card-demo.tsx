e42ef4ba8e2beb89a8c4d7039e6e9612e9a07387
Dillion Verma <hello@dillionverma.com>
2023-10-30 20:04:29 -0700

    🐛 fix: add shadows

--- registry/components/example/tweet-card-demo.tsx
+++ registry/components/example/tweet-card-demo.tsx
@@ -1,5 +1,5 @@
 import TweetCard from "@/registry/components/magicui/tweet-card";
 
 export default async function TweetDemo() {
-  return <TweetCard id="1668408059125702661" />;
+  return <TweetCard id="1668408059125702661" className="shadow-2xl" />;
 }
