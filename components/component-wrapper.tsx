55672224144abf3dbccfcab6e92f646d522ad5d6
Dillion Verma <hello@dillionverma.com>
2023-11-02 02:35:36 -0700

    🐛 fix: update wrapper styles

--- components/component-wrapper.tsx
+++ components/component-wrapper.tsx
@@ -14,10 +14,9 @@ const ComponentWrapper = ({ className, children }: ComponentWrapperProps) => {
     >
       <div
         className={cn(
-          `absolute inset-0 h-full w-full [background-size:16px_16px]`,
-          `bg-[radial-gradient(#00000055_1px,transparent_1px)] [--mask-offset:60px] dark:bg-[radial-gradient(#ffffff22_1px,transparent_1px)]`,
-          `[WebkitMask:mask:linear-gradient(to_bottom,transparent,#fff_var(--mask-offset)_calc(100%-var(--mask-offset)),transparent),linear-gradient(to_right,transparent,#fff_var(--mask-offset)_calc(100%-var(--mask-offset)),transparent)] [mask:linear-gradient(to_bottom,transparent,#fff_var(--mask-offset)_calc(100%-var(--mask-offset)),transparent),linear-gradient(to_right,transparent,#fff_var(--mask-offset)_calc(100%-var(--mask-offset)),transparent)]`,
-          `[--webkit-mask-composite:source-in,xor] [mask-composite:intersect]`,
+          `absolute inset-0 h-full w-full`,
+          `bg-[radial-gradient(#00000055_1px,transparent_1px)] dark:bg-[radial-gradient(#ffffff22_1px,transparent_1px)]`,
+          "lab-bg [background-size:16px_16px]",
         )}
       />
       {children}
