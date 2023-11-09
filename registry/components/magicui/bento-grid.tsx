a6f20e0d5b8e80b6e5af076fb22a2a7f2c207a97
Dillion Verma <hello@dillionverma.com>
2023-11-09 15:42:42 -0500

    ✨ feat: improved dark styles

--- registry/components/magicui/bento-grid.tsx
+++ registry/components/magicui/bento-grid.tsx
@@ -40,7 +40,11 @@ const BentoCard = ({
   <div
     key={name}
     className={cn(
-      "group relative flex flex-col justify-between overflow-hidden rounded-xl bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] dark:bg-neutral-900",
+      "group relative flex flex-col justify-between overflow-hidden rounded-xl",
+      // light styles
+      "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+      // dark styles
+      "dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
       span,
     )}
   >
