c5d4f177eede8be575bb3c78c32c2e05f8fd27e0
Dillion Verma <hello@dillionverma.com>
2023-11-17 01:59:49 -0500

    🐛 fix: safari mask bug fix

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -37,7 +37,7 @@ export default function Hero() {
             <div
               className={cn(
                 `absolute inset-0 block h-full w-full animate-gradient bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 bg-[length:var(--bg-size)_100%] p-[1px] [border-radius:inherit] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]`,
-                `[--webkit-mask-composite:xor] [mask-composite:xor]`,
+                `[-webkit-mask-composite:xor] [mask-composite:xor]`,
               )}
             />
           </Link>
