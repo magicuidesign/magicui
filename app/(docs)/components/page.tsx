7565b8256d424b1e0731031c4190cf236fb037f1
Dillion Verma <hello@dillionverma.com>
2023-11-18 20:06:40 -0500

    🐛 fix: fix font

--- app/(docs)/components/page.tsx
+++ app/(docs)/components/page.tsx
@@ -21,7 +21,7 @@ export default async function ComponentPage() {
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          <h1 className="inline-block text-4xl font-medium lg:text-5xl">
+          <h1 className="inline-block text-4xl font-medium tracking-tighter lg:text-5xl">
             Components
           </h1>
           <p className="text-xl text-muted-foreground">
