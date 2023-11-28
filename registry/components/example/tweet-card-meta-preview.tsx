03f962caaaf709d2ad1563be54b9bb68b2e1e6f3
Dillion Verma <hello@dillionverma.com>
2023-11-27 21:08:26 -0500

    🐛 fix: update class name

--- registry/components/example/tweet-card-meta-preview.tsx
+++ registry/components/example/tweet-card-meta-preview.tsx
@@ -1,5 +1,5 @@
 import TweetCard from "@/registry/components/magicui/tweet-card";
 
-export default async function TweetImages() {
+export default async function TweetMetaPreview() {
   return <TweetCard id="1675849118445436929" className="shadow-2xl" />;
 }
