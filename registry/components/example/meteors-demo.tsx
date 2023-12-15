7929d8f9d23cb9bd1b6f9e619477fb081b8ea03f
Dillion Verma <hello@dillionverma.com>
2023-12-15 16:44:30 -0500

    🐛 fix: update meteor design

--- registry/components/example/meteors-demo.tsx
+++ registry/components/example/meteors-demo.tsx
@@ -3,11 +3,10 @@ import Meteors from "@/registry/components/magicui/meteors";
 export default function MeteorDemo() {
   return (
     <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-20 shadow-2xl">
-      <Meteors />
+      <Meteors number={30} />
       <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
         Meteors
       </p>
-      <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
     </div>
   );
 }
