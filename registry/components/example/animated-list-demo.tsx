a863294d6f11a23d7c96c634e94e28b59552bb16
Dillion Verma <hello@dillionverma.com>
2023-12-14 20:56:39 -0500

    🐛 fix: better responsive

--- registry/components/example/animated-list-demo.tsx
+++ registry/components/example/animated-list-demo.tsx
@@ -58,7 +58,7 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
         "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
       )}
     >
-      <div className="flex flex-row items-center gap-4">
+      <div className="flex flex-row items-center gap-3">
         <div
           className="flex h-10 w-10 items-center justify-center rounded-2xl"
           style={{
@@ -67,9 +67,9 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
         >
           <span className="text-lg">{icon}</span>
         </div>
-        <div className="flex flex-col">
-          <figcaption className="flex flex-row items-center text-lg font-medium dark:text-white">
-            <span>{name}</span>
+        <div className="flex flex-col overflow-hidden">
+          <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white ">
+            <span className="text-sm sm:text-lg">{name}</span>
             <span className="mx-1">·</span>
             <span className="text-xs text-gray-500">{time}</span>
           </figcaption>
