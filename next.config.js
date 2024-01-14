8390dd17a2f66983f64e6e615d326ae86552dca7
Dillion Verma <hello@dillionverma.com>
2024-01-13 21:06:18 -0500

    🐛 fix: update build maps and rmove production logs

--- next.config.js
+++ next.config.js
@@ -4,8 +4,14 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  productionBrowserSourceMaps: true,
   experimental: {
     optimizeCss: true,
+  },
+  compiler: {
+    // Remove console.log from production except for error logs
+    removeConsole:
+      process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
   },
   images: {
     domains: ["localhost", "cdn.magicui.design"],
