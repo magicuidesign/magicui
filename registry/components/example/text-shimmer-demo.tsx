edf11bcff56edd47b9e781da483d9cc6b71bbaec
Dillion Verma <hello@dillionverma.com>
2024-01-20 01:27:27 -0500

    🐛 fix: update shimmer to show

--- registry/components/example/text-shimmer-demo.tsx
+++ registry/components/example/text-shimmer-demo.tsx
@@ -4,12 +4,10 @@ import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 export default async function TextShimmerDemo() {
   return (
-    <div className="min-h-[16rem] items-center flex justify-center">
+    <div className="z-10 flex min-h-[16rem] items-center justify-center">
       <div
         className={cn(
-          "rounded-full border dark:border-white/5 border-black/5 dark:bg-neutral-900 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer dark:hover:bg-neutral-800 hover:bg-neutral-200",
-          "group gap-1",
-          "translate-y-[-1rem] animate-fade-in opacity-0",
+          "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
         )}
       >
         <TextShimmer className="inline-flex items-center justify-center px-4 py-1">
@@ -17,6 +15,6 @@ export default async function TextShimmerDemo() {
           <ArrowRightIcon className="size-3 ml-1 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
         </TextShimmer>
       </div>
-      </div>
+    </div>
   );
 }
