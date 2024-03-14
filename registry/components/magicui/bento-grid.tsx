6a31b45438ce3b99f134d215a0673997cf082008
Dillion Verma <hello@dillionverma.com>
2024-03-14 10:33:31 -0400

    fix: make bento grid background black

--- registry/components/magicui/bento-grid.tsx
+++ registry/components/magicui/bento-grid.tsx
@@ -46,7 +46,7 @@ const BentoCard = ({
       // light styles
       "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
       // dark styles
-      "transform-gpu dark:bg-transparent dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+      "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
       className,
     )}
   >
