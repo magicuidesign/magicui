e0490ecdee0e099e516564a90491298133728a44
Dillion Verma <hello@dillionverma.com>
2024-03-24 22:04:26 -0400

    fix: update text-shimmer styles

--- registry/components/magicui/text-shimmer.tsx
+++ registry/components/magicui/text-shimmer.tsx
@@ -26,7 +26,7 @@ const TextShimmer: FC<TextShimmerProps> = ({
         "animate-shimmer bg-clip-text bg-no-repeat [background-position:0_0] [background-size:var(--shimmer-width)_100%] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite]",
 
         // Shimmer gradient
-        "bg-gradient-to-r from-neutral-100 via-black/80 via-50% to-neutral-100 dark:from-neutral-900 dark:via-white/80 dark:to-neutral-900",
+        "bg-gradient-to-r from-transparent via-black/80 via-50% to-transparent  dark:via-white/80",
 
         className,
       )}
