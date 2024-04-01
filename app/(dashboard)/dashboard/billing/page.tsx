1763c527767cc6cdd4f56e50190eb7f1182a6ed7
Dillion Verma <hello@dillion.io>
2024-04-01 15:47:35 -0400

    fix: add feature to generate invoice

--- app/(dashboard)/dashboard/billing/page.tsx
+++ app/(dashboard)/dashboard/billing/page.tsx
@@ -1,8 +1,11 @@
 import { DashboardHeader } from "@/components/dashboard/header";
 import { DashboardShell } from "@/components/dashboard/shell";
 import { BillingPortalButton } from "@/components/stripe-portal-button";
+import { buttonVariants } from "@/components/ui/button";
 import { authOptions } from "@/lib/auth";
 import { getCurrentUser } from "@/lib/session";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export const metadata = {
@@ -21,6 +24,14 @@ export default async function BillingPage() {
     <DashboardShell>
       <DashboardHeader heading="Billing" text="Manage billing information" />
       <BillingPortalButton>Manage Billing</BillingPortalButton>
+      <Link
+        href={`https://zenvoice.io/p/65d4ce3bbba9ed59ff11c076?email=${user.email}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={cn(buttonVariants(), "flex w-full max-w-[400px] gap-2")}
+      >
+        Generate Invoice
+      </Link>
       {/* <div className="grid gap-8">
         <OrderList payments={payments} />
       </div> */}
