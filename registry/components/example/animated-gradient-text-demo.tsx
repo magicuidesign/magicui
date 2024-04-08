ceb4903d6325c21a82040749da002afcb1f7e8dd
Dillion Verma <hello@dillion.io>
2024-04-08 18:18:28 -0400

    fix: update shiny text styles

--- registry/components/example/animated-gradient-text-demo.tsx
+++ registry/components/example/animated-gradient-text-demo.tsx
@@ -6,7 +6,7 @@ export default async function AnimatedGradientTextDemo() {
   return (
     <div className="z-10 flex min-h-[16rem] items-center justify-center">
       <AnimatedGradientText>
-        🎉 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-border" />{" "}
+        🎉 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
         <span
           className={cn(
             `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
@@ -14,7 +14,7 @@ export default async function AnimatedGradientTextDemo() {
         >
           Introducing Magic UI
         </span>
-        <ChevronRight className="ml-1 h-4 w-4 text-gray-500" />
+        <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
       </AnimatedGradientText>
     </div>
   );
--- registry/components/example/animated-shiny-text-demo.tsx
+++ registry/components/example/animated-shiny-text-demo.tsx
@@ -8,9 +8,10 @@ export default async function TextShimmerDemo() {
       <div
         className={cn(
           "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
+          "",
         )}
       >
-        <TextShimmer className="inline-flex items-center justify-center px-4 py-1">
+        <TextShimmer className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
           <span>✨ Introducing Magic UI</span>
           <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
         </TextShimmer>
