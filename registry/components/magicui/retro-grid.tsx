9f5b030f948512269959ed6a075abdde96b785e6
Dillion Verma <hello@dillion.io>
2024-04-12 20:48:33 -0400

    fix: update retrogrid styles

--- registry/components/magicui/retro-grid.tsx
+++ registry/components/magicui/retro-grid.tsx
@@ -9,12 +9,12 @@ export default function RetroGrid({ className }: { className?: string }) {
       )}
     >
       {/* Grid */}
-      <div className="absolute inset-0 [transform:rotateX(45deg)]">
+      <div className="absolute inset-0 [transform:rotateX(35deg)]">
         <div
           className={cn(
             "animate-grid",
 
-            "[background-repeat:repeat] [background-size:60px_60px] [height:300vh] [inset:0%_0px] [margin-left:-50%] [transform-origin:100%_0_0] [width:200vw]",
+            "[background-repeat:repeat] [background-size:60px_60px] [height:300vh] [inset:0%_0px] [margin-left:-50%] [transform-origin:100%_0_0] [width:600vw]",
 
             // Light Styles
             "[background-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1px,transparent_0),linear-gradient(to_bottom,rgba(0,0,0,0.3)_1px,transparent_0)]",
