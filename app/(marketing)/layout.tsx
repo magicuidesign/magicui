5b280d71356de9d67c80de5f0467974febf4ce3a
Dillion Verma <hello@dillion.io>
2024-04-18 02:34:54 -0400

    fix: add site banner

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,4 @@
+import { SiteBanner } from "@/components/site-banner";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 import { getCurrentUser } from "@/lib/session";
@@ -13,7 +14,7 @@ export default async function MarketingLayout({
 
   return (
     <>
-      {/* <SiteBanner /> */}
+      <SiteBanner />
       <SiteHeader user={user} />
       <main className="flex-1">{children}</main>
       <SiteFooter />
--- components/site-banner.tsx
+++ components/site-banner.tsx
@@ -5,6 +5,26 @@ export function SiteBanner() {
     <div className="relative top-0 bg-background py-3 md:py-0">
       <div className="container flex flex-col items-center justify-center gap-4 md:h-12 md:flex-row">
         <Link
+          href="/discord"
+          className="text-center text-sm leading-loose text-muted-foreground"
+        >
+          ✨
+          <span className="font-bold text-black dark:text-white">
+            {" "}
+            Join our new discord community <strong>here</strong>
+          </span>{" "}
+          ✨
+          {/* Get{" "}
+          <span className="font-bold text-black dark:text-white">
+            $30 off
+          </span>{" "}
+          with code{" "}
+          <span className="rounded-md bg-orange-400 p-1 font-bold text-black dark:text-white">
+            BLOCKPARTY
+          </span>{" "}
+          ✨ */}
+        </Link>
+        {/* <Link
           href="/pricing"
           className="text-center text-sm leading-loose text-muted-foreground"
         >
@@ -22,7 +42,7 @@ export function SiteBanner() {
             BLOCKPARTY
           </span>{" "}
           ✨
-        </Link>
+        </Link> */}
       </div>
       <hr className="absolute bottom-0 m-0 h-px w-full bg-neutral-200/30" />
     </div>
