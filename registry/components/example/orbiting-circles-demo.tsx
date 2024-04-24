611598943c225d9aa5103311b8eb3cca0d5aea06
Dillion Verma <hello@dillion.io>
2024-04-24 15:13:24 -0400

    fix: update orbit path

--- registry/components/example/orbiting-circles-demo.tsx
+++ registry/components/example/orbiting-circles-demo.tsx
@@ -1,9 +1,9 @@
 import OrbitingCircles from "@/registry/components/magicui/orbiting-circles";
-import { BarChartIcon, LinkIcon } from "lucide-react";
+import { AlignJustify, BarChartIcon, LinkIcon, Star } from "lucide-react";
 
 export default function OrbitingCirclesDemo() {
   return (
-    <div className="relative flex h-[400px] w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+    <div className="relative flex h-[500px] w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Circles
       </span>
@@ -30,19 +30,19 @@ export default function OrbitingCirclesDemo() {
       <OrbitingCircles
         className="h-[50px] w-[50px]"
         reverse
-        radius={160}
+        radius={190}
         duration={20}
       >
-        hello
+        <Star className="text-black dark:text-white" />
       </OrbitingCircles>
       <OrbitingCircles
         className="h-[50px] w-[50px]"
         reverse
-        radius={160}
+        radius={190}
         duration={20}
         delay={20}
       >
-        world
+        <AlignJustify className="text-black dark:text-white" />
       </OrbitingCircles>
     </div>
   );
