108bf809b9bbdb066a3bf4071d425367dafd24d3
Dillion Verma <hello@dillion.io>
2024-04-26 16:18:01 -0400

    fix: remove empty quotes

--- registry/components/example/animated-shiny-text-demo.tsx
+++ registry/components/example/animated-shiny-text-demo.tsx
@@ -8,7 +8,6 @@ export default async function TextShimmerDemo() {
       <div
         className={cn(
           "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
-          "",
         )}
       >
         <TextShimmer className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
