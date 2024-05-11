0cf56be45aee13227d13d4da5cd9f438bf0f8d60
Dillion Verma <hello@dillion.io>
2024-05-10 23:37:16 -0400

    fix: update posthog on prod

--- components/posthog-provider.tsx
+++ components/posthog-provider.tsx
@@ -5,7 +5,7 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { useEffect } from "react";
 
-if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
+if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY!, {
     api_host: "https://app.posthog.com",
     session_recording: {
