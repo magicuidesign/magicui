8821c3f88157fe1bfd172ba95ac4b83dfda7b818
Dillion Verma <hello@dillion.io>
2024-05-16 18:13:51 -0400

    fix: env vars

--- env.mjs
+++ env.mjs
@@ -6,26 +6,7 @@ export const env = createEnv({
    * Serverside Environment variables, not available on the client.
    * Will throw if you access these variables on the client.
    */
-  server: {
-    // This is optional because it's only used in development.
-    // See https://next-auth.js.org/deployment.
-    NEXTAUTH_URL: z.string().url().optional(),
-    NEXTAUTH_SECRET: z.string().min(1),
-    // GITHUB_CLIENT_ID: z.string().min(1),
-    // GITHUB_CLIENT_SECRET: z.string().min(1),
-    // GITHUB_ACCESS_TOKEN: z.string().min(1),
-    DATABASE_URL: z.string().min(1),
-    RESEND_API_KEY: z.string().optional(),
-    STRIPE_API_KEY: z.string().optional(),
-    STRIPE_WEBHOOK_SECRET: z.string().optional(),
-    // SMTP_FROM: z.string().min(1),
-    // POSTMARK_API_TOKEN: z.string().min(1),
-    // POSTMARK_SIGN_IN_TEMPLATE: z.string().min(1),
-    // POSTMARK_ACTIVATION_TEMPLATE: z.string().min(1),
-    // STRIPE_API_KEY: z.string().min(1),
-    // STRIPE_WEBHOOK_SECRET: z.string().min(1),
-    // STRIPE_PRO_MONTHLY_PLAN_ID: z.string().min(1),
-  },
+  server: {},
   /*
    * Environment variables available on the client (and server).
    *
@@ -33,8 +14,6 @@ export const env = createEnv({
    */
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
-    NEXT_PUBLIC_STRIPE_PAYMENT_LINK: z.string().min(1),
-    NEXT_PUBLIC_POSTHOG_API_KEY: z.string().optional(),
   },
 
   /*
@@ -44,26 +23,6 @@ export const env = createEnv({
    * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
    */
   runtimeEnv: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    // GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    // GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-    // GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
-    DATABASE_URL: process.env.DATABASE_URL,
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-    // SMTP_FROM: process.env.SMTP_FROM,
-    // POSTMARK_API_TOKEN: process.env.POSTMARK_API_TOKEN,
-    // POSTMARK_SIGN_IN_TEMPLATE: process.env.POSTMARK_SIGN_IN_TEMPLATE,
-    // POSTMARK_ACTIVATION_TEMPLATE: process.env.POSTMARK_ACTIVATION_TEMPLATE,
-    // STRIPE_API_KEY: process.env.STRIPE_API_KEY,
-    // STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-    // STRIPE_PRO_MONTHLY_PLAN_ID: process.env.STRIPE_PRO_MONTHLY_PLAN_ID,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-    NEXT_PUBLIC_STRIPE_PAYMENT_LINK:
-      process.env.NEXT_PUBLIC_STRIPE_PAYMENT_LINK,
-
-    NEXT_PUBLIC_POSTHOG_API_KEY: process.env.NEXT_PUBLIC_POSTHOG_API_KEY,
   },
 });
