3caf691baae7f02ba90902578b59073094578ae5
Dillion Verma <hello@dillion.io>
2024-05-18 22:37:52 -0400

    fix: remove cta

--- components/landing/cta-section.tsx
+++ components/landing/cta-section.tsx
@@ -1,84 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import Marquee from "@/registry/components/magicui/marquee";
 import { ChevronRight, HeartHandshake } from "lucide-react";
-
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
-];
-
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
-
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  body,
-}: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
-}) => {
-  return (
-    <figure
-      className={cn(
-        "relative w-64 cursor-pointer overflow-hidden rounded-[2rem] border p-4",
-        // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-      )}
-    >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
-    </figure>
-  );
-};
 
 export default function CTASection() {
   return (
@@ -86,53 +8,6 @@ export default function CTASection() {
       <div className="py-14">
         <div className="container flex w-full flex-col items-center justify-center p-4">
           <div className="relative flex w-full max-w-[1000px] flex-col items-center justify-center overflow-hidden rounded-[2rem] border p-10 py-14">
-            <div className="absolute rotate-[35deg]">
-              <Marquee pauseOnHover className="[--duration:20s]" repeat={3}>
-                {firstRow.map((review) => (
-                  <ReviewCard key={review.username} {...review} />
-                ))}
-              </Marquee>
-              <Marquee
-                reverse
-                pauseOnHover
-                className="[--duration:20s]"
-                repeat={3}
-              >
-                {secondRow.map((review) => (
-                  <ReviewCard key={review.username} {...review} />
-                ))}
-              </Marquee>
-              <Marquee pauseOnHover className="[--duration:20s]" repeat={3}>
-                {firstRow.map((review) => (
-                  <ReviewCard key={review.username} {...review} />
-                ))}
-              </Marquee>
-              <Marquee
-                reverse
-                pauseOnHover
-                className="[--duration:20s]"
-                repeat={3}
-              >
-                {secondRow.map((review) => (
-                  <ReviewCard key={review.username} {...review} />
-                ))}
-              </Marquee>
-              <Marquee pauseOnHover className="[--duration:20s]" repeat={3}>
-                {firstRow.map((review) => (
-                  <ReviewCard key={review.username} {...review} />
-                ))}
-              </Marquee>
-              <Marquee
-                reverse
-                pauseOnHover
-                className="[--duration:20s]"
-                repeat={3}
-              >
-                {secondRow.map((review) => (
-                  <ReviewCard key={review.username} {...review} />
-                ))}
-              </Marquee>
-            </div>
             <div className="z-10 mx-auto size-24 rounded-[2rem] border bg-white/10 p-3 shadow-2xl backdrop-blur-md dark:bg-black/10 lg:size-32">
               <HeartHandshake className="mx-auto size-16 text-black dark:text-white lg:size-24" />
             </div>
