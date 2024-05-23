fb08de5bbfe20cffd3142999967cdc6868499898
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2024-05-24 00:25:06 +0100

    feat: add blur in text component (#18)

---
+++ registry/components/example/blur-in-demo.tsx
@@ -0,0 +1,10 @@
+import BlurIn from "../magicui/blur-in";
+const BlurInDemo = () => {
+  return (
+    <BlurIn
+      word="Blur In"
+      className="text-4xl font-bold text-black dark:text-white"
+    />
+  );
+};
+export default BlurInDemo;
