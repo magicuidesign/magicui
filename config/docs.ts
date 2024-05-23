b20bb3dde40fed22a8a5ab4ca6801c2f88d0f245
tomonarifeehan <feehan.tomonari@gmail.com>
2024-05-22 21:50:36 -0400

    revert commit

--- config/docs.ts
+++ config/docs.ts
@@ -83,12 +83,6 @@ export const docsConfig: DocsConfig = {
           items: [],
           label: "New",
         },
-        {
-          title: "Avatar Circles",
-          href: `/docs/components/avatar-circles`,
-          items: [],
-          label: "New",
-        },
       ],
     },
     {
