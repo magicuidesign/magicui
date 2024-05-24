94ac6bf01c693300d5a3dc51fb47767c2e4cf789
Dillion Verma <hello@dillion.io>
2024-05-23 22:14:27 -0400

    fix: style fixes

--- registry/components/magicui/avatar-circles.tsx
+++ registry/components/magicui/avatar-circles.tsx
@@ -3,21 +3,34 @@
 import { cn } from "@/lib/utils";
 
 interface AvatarCirclesProps {
-    className?: string;
-    numPeople?: number;
+  className?: string;
+  numPeople?: number;
 }
 
 export default function AvatarCircles({
-    numPeople,
-    className,
+  numPeople,
+  className,
 }: AvatarCirclesProps) {
-
   return (
-    <div className = { cn("flex -space-x-4 rtl:space-x-reverse", className) }>
-        <img className = "w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src = "https://avatars.githubusercontent.com/u/16860528" alt = ""/>
-        <img className = "w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src = "https://avatars.githubusercontent.com/u/29210732" alt = ""/>
-        <img className = "w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src = "https://avatars.githubusercontent.com/u/20110627" alt = ""/>
-        <a className = "flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href = "#">+{ numPeople }</a>
+    <div className={cn("z-10 flex -space-x-4 rtl:space-x-reverse", className)}>
+      <img
+        className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+        src="https://avatars.githubusercontent.com/u/16860528"
+        alt=""
+      />
+      <img
+        className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+        src="https://avatars.githubusercontent.com/u/29210732"
+        alt=""
+      />
+      <img
+        className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+        src="https://avatars.githubusercontent.com/u/20110627"
+        alt=""
+      />
+      <a className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black">
+        +{numPeople}
+      </a>
     </div>
   );
 }
