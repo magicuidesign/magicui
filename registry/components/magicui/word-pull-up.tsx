9207031a92d385db31530739c9523befec6d4921
Gökhan Öztürk <gokhanozturk@teknasyon.com>
2024-05-24 05:24:11 +0300

    Add Word Pull Up component (#40)
    * feat: Add Word Pull Up component
    
    * fix: small fixes
    
    ---------
    
    Co-authored-by: Dillion Verma <hello@dillion.io>

---
+++ registry/components/magicui/word-pull-up.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { motion, Variants } from "framer-motion";
+import { cn } from "lib/utils";
+
+interface WordPullUpProps {
+  words: string;
+  duration?: number;
+  delayMultiple?: number;
+  wrapperFramerProps?: Variants;
+  framerProps?: Variants;
+  className?: string;
+}
+
+export default function WordPullUp({
+  words,
+  wrapperFramerProps = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  },
+  framerProps = {
+    hidden: { y: 20, opacity: 0 },
+    show: { y: 0, opacity: 1 },
+  },
+  className,
+}: WordPullUpProps) {
+  return (
+    <motion.h1
+      variants={wrapperFramerProps}
+      initial="hidden"
+      animate="show"
+      className={cn(
+        "font-display text-center text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]",
+        className,
+      )}
+    >
+      {words.split(" ").map((word, i) => (
+        <motion.span
+          key={i}
+          variants={framerProps}
+          style={{ display: "inline-block", paddingRight: "15px" }}
+        >
+          {word === "" ? <span>&nbsp;</span> : word}
+        </motion.span>
+      ))}
+    </motion.h1>
+  );
+}
