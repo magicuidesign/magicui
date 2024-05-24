e859ba7be35530ba2dace1f86ca9d5816d1e5c44
luis-tenorio-code <luisprograming0@gmail.com>
2024-05-24 13:51:15 -0500

    chore: Refactor shiny button component for improved performance

--- registry/components/magicui/shiny-button.tsx
+++ registry/components/magicui/shiny-button.tsx
@@ -24,8 +24,26 @@ const animationProps = {
 
 const ShinyButton = ({ text = "shiny-button" }) => {
   return (
-    <motion.button {...animationProps} className="shiny__Button">
-      <span className="shiny__Button--text">{text}</span>
+    <motion.button
+      {...animationProps}
+      className="relative rounded-lg px-6 py-2 font-medium transition-[box-shadow] duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)] backdrop-blur-xl"
+    >
+      <span
+        className="relative block h-full w-full text-sm uppercase tracking-wide text-[rgb(0,0,0,65%)] dark:font-light dark:text-[rgb(255,255,255,90%)]"
+        style={{
+          maskImage:
+            "linear-gradient(-75deg,hsl(var(--primary)) calc(var(--x) + 20%),transparent calc(var(--x) + 30%),hsl(var(--primary)) calc(var(--x) + 100%))",
+        }}
+      >
+        {text}
+      </span>
+      <span
+        style={{
+          mask: "linear-gradient(rgb(0,0,0), rgb(0,0,0)) content-box,linear-gradient(rgb(0,0,0), rgb(0,0,0))",
+          maskComposite: "exclude",
+        }}
+        className="absolute inset-0 z-10 block rounded-[inherit] bg-[linear-gradient(-75deg,hsl(var(--primary)/10%)_calc(var(--x)+20%),hsl(var(--primary)/50%)_calc(var(--x)+25%),hsl(var(--primary)/10%)_calc(var(--x)+100%))] p-px"
+      ></span>
     </motion.button>
   );
 };
