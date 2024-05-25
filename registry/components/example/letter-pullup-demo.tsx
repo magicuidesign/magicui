e28a4c4fab1f4b5ac43652772d39440946fd5481
santisemhan <santisemhan2@gmail.com>
2024-05-24 22:03:20 -0300

    fix: text visible in dark mode in Letter PullUp component

--- registry/components/example/letter-pullup-demo.tsx
+++ registry/components/example/letter-pullup-demo.tsx
@@ -3,7 +3,7 @@ import LetterPullup from "@/registry/components/magicui/letter-pullup";
  export default async function LetterPullupDemo() {
    return (
      <LetterPullup
-       className = "text-black"
+       className = "text-black dark:text-white"
        words = { "Staggered Letter Pull Up" }
        delay = { 0.05 }
      />
