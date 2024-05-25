d90c1ca733cadf6a83839cd214dda0c3cd12ebe1
Dillion Verma <hello@dillion.io>
2024-05-24 22:14:27 -0400

    fix: color

--- registry/components/magicui/word-fade-in.tsx
+++ registry/components/magicui/word-fade-in.tsx
@@ -31,7 +31,7 @@ export default function WordFadeIn({
       initial="hidden"
       animate="visible"
       className={cn(
-        "font-display text-center text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]",
+        "font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]",
         className,
       )}
     >
