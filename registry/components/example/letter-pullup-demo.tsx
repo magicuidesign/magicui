d4078baf7df06cc1966b83f5f65b57bcc4519297
santisemhan <58712215+santisemhan@users.noreply.github.com>
2024-05-24 23:09:27 -0300

    fix: text visible in dark mode in Letter PullUp component (#63)
    * fix: text visible in dark mode in Letter PullUp component
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Dillion Verma <hello@dillion.io>

--- registry/components/example/letter-pullup-demo.tsx
+++ registry/components/example/letter-pullup-demo.tsx
@@ -1,11 +1,11 @@
 import LetterPullup from "@/registry/components/magicui/letter-pullup";
 
- export default async function LetterPullupDemo() {
-   return (
-     <LetterPullup
-       className = "text-black"
-       words = { "Staggered Letter Pull Up" }
-       delay = { 0.05 }
-     />
-   );
- }
+export default async function LetterPullupDemo() {
+  return (
+    <LetterPullup
+      className="text-black dark:text-white"
+      words={"Staggered Letter Pull Up"}
+      delay={0.05}
+    />
+  );
+}
