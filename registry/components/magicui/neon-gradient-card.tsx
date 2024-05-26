386d4c01930f0b7527a64551b6782d981e3b7c5e
Draxx0 <williamfort.lmgl@gmail.com>
2024-05-26 17:51:32 +0200

    :fix update the card default bg color on light mode

--- registry/components/magicui/neon-gradient-card.tsx
+++ registry/components/magicui/neon-gradient-card.tsx
@@ -122,7 +122,7 @@ const NeonGradientCard: React.FC<NeonGradientCardProps> = ({
     >
       <div
         className={cn(
-          "relative h-full min-h-[inherit] w-full rounded-[var(--card-content-radius)] bg-gray-300 p-6",
+          "relative h-full min-h-[inherit] w-full rounded-[var(--card-content-radius)] bg-gray-100 p-6",
           "before:absolute before:-left-[var(--border-size)] before:-top-[var(--border-size)] before:-z-10 before:block",
           "before:h-[var(--pseudo-element-height)] before:w-[var(--pseudo-element-width)] before:rounded-[var(--border-radius)] before:content-['']",
           "before:bg-[linear-gradient(0deg,var(--neon-first-color),var(--neon-second-color))] before:bg-[length:100%_200%]",
