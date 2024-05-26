f332d2d862499c4f31c3ec85af3a29203bea1370
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-05-26 22:42:28 +0530

    fix: path change to examples

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,24 +1,16 @@
-import { User } from "@prisma/client";
 import { AvatarProps } from "@radix-ui/react-avatar";
 
 import { Icons } from "@/components/icons";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-interface UserAvatarProps extends AvatarProps {
-  user: Pick<User, "image" | "name">;
-}
-
-export function UserAvatar({ user, ...props }: UserAvatarProps) {
+export function UserAvatar({ ...props }: AvatarProps) {
   return (
     <Avatar {...props}>
-      {user.image ? (
-        <AvatarImage alt="Picture" src={user.image} />
-      ) : (
+        <AvatarImage alt="Picture" src={"https://github.com/dillionverma"} />
         <AvatarFallback>
-          <span className="sr-only">{user.name}</span>
+          <span className="sr-only">{"Dillion Verma"}</span>
           <Icons.user className="h-4 w-4" />
         </AvatarFallback>
-      )}
     </Avatar>
   );
 }
