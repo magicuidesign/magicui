9d8fb67fc91e0fa33d61545a5b0e8f40c7fc97d1
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-05-26 18:28:52 +0530

    feat: Magic UI CLI

---
+++ packages/cli/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  clean: true,
+  dts: false,
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  sourcemap: true,
+  minify: true,
+  target: "esnext",
+  outDir: "dist",
+})
