935d215a0b7f0e52a8cd1eb89943c1df75e7eb02
safethecode <iam@sonjim.in>
2024-06-01 13:48:15 +0900

    chore: typo edits

--- registry/components/example/tweet-card-demo.tsx
+++ registry/components/example/tweet-card-demo.tsx
@@ -1,8 +1,8 @@
 import ClientTweetCard from "@/registry/components/magicui/client-tweet-card";
 
 /**
- * TweetCard (Server Side Only)
- * ClientTweetCard (Client Side Only)
+ * <TweetCard /> (Server Side Only)
+ * <ClientTweetCard /> (Client Side Only)
  */
 export default async function TweetDemo() {
   return <ClientTweetCard id="1668408059125702661" className="shadow-2xl" />;
