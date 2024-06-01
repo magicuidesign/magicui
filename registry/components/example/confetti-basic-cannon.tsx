6f9315627c5dd3f43128283b712286c9ddc6cf71
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-06-01 17:20:40 -0400

    confetti examples

---
+++ registry/components/example/confetti-basic-cannon.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import {Confetti} from "@/registry/components/magicui/confetti";
+import React from "react";
+
+export default function ConfettiBasicCannon() {
+  const handleClick = () => {
+    Confetti({}); // Use default settings for basic confetti
+  };
+
+  return (
+    <div className="relative justify-center">
+      <Button onClick={handleClick}>Trigger Basic Cannon</Button>
+    </div>
+  );
+}
---
+++ registry/components/example/confetti-random-direction.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import {Confetti} from "@/registry/components/magicui/confetti";
+import React from "react";
+
+export default function ConfettiRandomDirection() {
+  const handleClick = () => {
+    Confetti({
+      angle: Math.random() * 360,
+    });
+  };
+
+  return (
+    <div className="relative justify-center">
+      <Button onClick={handleClick}>Trigger Random Direction</Button>
+    </div>
+  );
+}
