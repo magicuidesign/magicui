8cae7de109c8a632bb931610a3018a3619056cf8
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-06-01 17:20:18 -0400

    confetti component

---
+++ registry/components/magicui/confetti.tsx
@@ -0,0 +1,53 @@
+// import confetti from "canvas-confetti";
+const confetti = require("canvas-confetti").default;
+
+interface ConfettiOptions {
+  particleCount?: number;
+  angle?: number;
+  spread?: number;
+  startVelocity?: number;
+  decay?: number;
+  gravity?: number;
+  drift?: number;
+  flat?: boolean;
+  ticks?: number;
+  origin?: { x: number; y: number };
+  colors?: string[];
+  shapes?: string[];
+  zIndex?: number;
+  disableForReducedMotion?: boolean;
+  useWorker?: boolean;
+  resize?: boolean;
+  canvas?: HTMLCanvasElement | null;
+  scalar?: number;
+}
+
+const Confetti = (options: ConfettiOptions) => {
+  if (
+    options.disableForReducedMotion &&
+    window.matchMedia("(prefers-reduced-motion)").matches
+  ) {
+    return;
+  }
+
+  const confettiInstance = options.canvas
+    ? confetti.create(options.canvas, {
+        resize: options.resize ?? true,
+        useWorker: options.useWorker ?? true,
+      })
+    : confetti;
+
+  confettiInstance({
+    ...options,
+  });
+};
+
+Confetti.shapeFromPath = (options: { path: string; [key: string]: any }) => {
+  return confetti.shapeFromPath({ ...options });
+};
+
+Confetti.shapeFromText = (options: { text: string; [key: string]: any }) => {
+  return confetti.shapeFromText({ ...options });
+};
+
+export { Confetti };
