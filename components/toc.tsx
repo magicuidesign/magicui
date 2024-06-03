14a34a48be43a04c0f31df57ac528048aed9c0a4
Gökhan Öztürk <gokhanozturk@teknasyon.com>
2024-06-04 00:10:59 +0300

    Update components/toc.tsx
    Co-authored-by: Jimin (Aaron) Son <iam@Sonjim.in>

--- components/toc.tsx
+++ components/toc.tsx
@@ -97,7 +97,7 @@ interface TreeProps {
 function Tree({ tree, level = 1, activeItem }: TreeProps) {
   return tree?.items?.length && level < 3 ? (
     <ul className={cn("m-0 list-none", { "pl-4": level !== 1 })}>
-      {tree.items.map((item, index) => {        
+      {tree.items.map((item, index) => {
         return (
           <li key={index} className={cn("mt-0 pt-2")}>
             <a
