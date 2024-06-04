2931d603793d5381e8bb04262b04768af9729898
Dillion Verma <hello@dillion.io>
2024-06-04 16:58:08 -0400

    fix: small fixes

--- components/landing/hero-client.tsx
+++ components/landing/hero-client.tsx
@@ -3,6 +3,7 @@
 import { Doc } from "@/.contentlayer/generated";
 import TechStack from "@/components/tech-stack";
 import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import AnimatedBeamMultipleInputDemo from "@/registry/components/example/animated-beam-multiple-inputs";
 import AnimatedListDemo from "@/registry/components/example/animated-list-demo";
@@ -33,7 +34,7 @@ export default function HeroClient({ post }: { post: Doc }) {
                       `p-[1px] ![mask-composite:subtract]`,
                     )}
                   />
-
+                  🎉 <Separator className="mx-2 h-4" orientation="vertical" />
                   <span
                     className={cn(
                       `animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
