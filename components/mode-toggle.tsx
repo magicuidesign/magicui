0191a90453b9d00ab1fb788d7f8941027f82563a
Arghya Das <arghyadasproject@gmail.com>
2024-06-05 13:47:14 +0530

    hero section oss badge position fix

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -10,15 +10,14 @@ export function ModeToggle() {
   return (
     <Button
       variant="ghost"
-      className="w-9 px-0"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      type="button"
+      size="icon"
+      className="px-2"
+      aria-label={"Toggle theme"}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "light" ? (
-        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      ) : (
-        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      )}
-      <span className="sr-only">Toggle theme</span>
+      <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
+      <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
     </Button>
   );
 }
