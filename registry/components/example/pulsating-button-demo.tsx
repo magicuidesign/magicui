181e1c1dd07d5b43201b093fb4009e0c6a2d2539
shikhap04 <the.shikha.patel@gmail.com>
2024-06-10 11:49:40 -0400

    updated button name

--- registry/components/example/pulsating-button-demo.tsx
+++ registry/components/example/pulsating-button-demo.tsx
@@ -3,12 +3,12 @@ import { PulsatingButton } from "../magicui/pulsating-button"
 
 export default function PulsatingButtonDemo() {
   return <PulsatingButton
-  text = 'Join Affiliate'
+  text = 'Join Affiliate Program'
   pulseColor = '0, 150, 255'
   backgroundColor= '#0096FF'
   textColor = '#ffffff'
   animationDuration = '2s'
-  buttonWidth = '125px'
+  buttonWidth = '200px'
   buttonHeight = '50px'
   />;
 }
