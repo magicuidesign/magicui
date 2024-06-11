c4c1f5e26735728be80cf14569bbf70e1c14548b
Ala <ala.menai@credium.de>
2024-06-11 22:10:45 +0100

    refactor: pass the avatars urls as props for component reusability

--- registry/components/example/avatar-circles-demo.tsx
+++ registry/components/example/avatar-circles-demo.tsx
@@ -1,5 +1,10 @@
 import AvatarCircles from "@/registry/components/magicui/avatar-circles";
 
+const avatarUrls = [
+  "https://avatars.githubusercontent.com/u/16860528",
+  "https://avatars.githubusercontent.com/u/20110627",
+];
+
 export default async function AvatarCirclesDemo() {
-  return <AvatarCircles numPeople={99} />;
+  return <AvatarCircles numPeople={99} avatarUrls={avatarUrls} />;
 }
--- registry/components/magicui/avatar-circles.tsx
+++ registry/components/magicui/avatar-circles.tsx
@@ -1,36 +1,35 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import React from "react";
 
 interface AvatarCirclesProps {
   className?: string;
   numPeople?: number;
+  avatarUrls: string[];
 }
 
-export default function AvatarCircles({
-  numPeople,
-  className,
-}: AvatarCirclesProps) {
+const AvatarCircles = ({ numPeople, className, avatarUrls }: AvatarCirclesProps) => {
   return (
     <div className={cn("z-10 flex -space-x-4 rtl:space-x-reverse", className)}>
-      <img
-        className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
-        src="https://avatars.githubusercontent.com/u/16860528"
-        alt=""
-      />
-      <img
-        className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
-        src="https://avatars.githubusercontent.com/u/29210732"
-        alt=""
-      />
-      <img
-        className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
-        src="https://avatars.githubusercontent.com/u/20110627"
-        alt=""
-      />
-      <a className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black">
+      {avatarUrls.map((url, index) => (
+        <img
+          key={index}
+          className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+          src={url}
+          width={40}
+          height={40}
+          alt={`Avatar ${index + 1}`}
+        />
+      ))}
+      <a
+        className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black"
+        href=""
+      >
         +{numPeople}
       </a>
     </div>
   );
-}
+};
+
+export default AvatarCircles;
