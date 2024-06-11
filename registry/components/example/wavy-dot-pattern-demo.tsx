c51c7012da53d3d39c3ecc00c116b64970820da6
Dillion Verma <hello@dillion.io>
2024-06-11 15:33:20 -0400

    fix: small fixes

--- registry/components/example/wavy-dot-pattern-demo.tsx
+++ registry/components/example/wavy-dot-pattern-demo.tsx
@@ -1,19 +1,18 @@
-"use client";
-
 import WavyDotPattern from "@/registry/components/magicui/wavy-dot-pattern";
 
-const WavyDotPatternDemo = () => {
+export default function WavyDotPatternDemo() {
   return (
-    <div className = "relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-background p-40">
-      
-      <p className = "z-10 whitespace-pre-wrap text-center text-4xl font-semibold tracking-tighter text-black dark:text-white">
+    <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl ">
+      <p className="z-10 whitespace-pre-wrap text-center text-4xl font-semibold tracking-tighter text-black dark:text-white">
         Wavy Dot Pattern
       </p>
-      
-      <WavyDotPattern gridWidth = { 30 } gridHeight = { 15 } dotWidth = { 8 } dotHeight = { 8 }/>
-    
-    </div>
-  )
-}
 
-export default WavyDotPatternDemo;
+      <WavyDotPattern
+        gridWidth={30}
+        gridHeight={15}
+        dotWidth={8}
+        dotHeight={8}
+      />
+    </div>
+  );
+}
