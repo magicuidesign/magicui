0991bb7678b8c727dfdc8ad2af4dbd73f4eb8034
Dillion Verma <hello@dillion.io>
2024-06-12 00:26:50 -0400

    fix: add more avatars

--- registry/components/example/avatar-circles-demo.tsx
+++ registry/components/example/avatar-circles-demo.tsx
@@ -3,6 +3,8 @@ import AvatarCircles from "@/registry/components/magicui/avatar-circles";
 const avatarUrls = [
   "https://avatars.githubusercontent.com/u/16860528",
   "https://avatars.githubusercontent.com/u/20110627",
+  "https://avatars.githubusercontent.com/u/106103625",
+  "https://avatars.githubusercontent.com/u/59228569",
 ];
 
 export default async function AvatarCirclesDemo() {
