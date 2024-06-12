2b85625e12768aee935d633e3dccefab095ecb2e
Ala Eddine Menai <ala.official.acc@gmail.com>
2024-06-12 05:29:11 +0100

    refactor: pass the avatars urls as props for component reusability (#146)
    * refactor: pass the avatars urls as props for component reusability
    
    * fix: add more avatars
    
    ---------
    
    Co-authored-by: Ala <ala.menai@credium.de>
    Co-authored-by: Dillion Verma <hello@dillion.io>

--- registry/components/example/avatar-circles-demo.tsx
+++ registry/components/example/avatar-circles-demo.tsx
@@ -1,5 +1,12 @@
 import AvatarCircles from "@/registry/components/magicui/avatar-circles";
 
+const avatarUrls = [
+  "https://avatars.githubusercontent.com/u/16860528",
+  "https://avatars.githubusercontent.com/u/20110627",
+  "https://avatars.githubusercontent.com/u/106103625",
+  "https://avatars.githubusercontent.com/u/59228569",
+];
+
 export default async function AvatarCirclesDemo() {
-  return <AvatarCircles numPeople={99} />;
+  return <AvatarCircles numPeople={99} avatarUrls={avatarUrls} />;
 }
