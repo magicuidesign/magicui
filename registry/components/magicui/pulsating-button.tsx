e7f1b7fe567e903656a193de322e1d6f6a6a3100
shikhap04 <the.shikha.patel@gmail.com>
2024-06-20 17:00:10 -0400

    fixed rounded edges

--- registry/components/magicui/pulsating-button.tsx
+++ registry/components/magicui/pulsating-button.tsx
@@ -24,7 +24,7 @@ export const PulsatingButton: React.FC<PulsatingButtonProps> = ({
 }) => {
   const pulseKeyframes = {
     '--tw-pulse-color': pulseColor,
-    animation: `pulse ${animationDuration} infinite`,
+    animation: `pulse ${animationDuration} linear infinite`,
   };
 
   return (
@@ -32,27 +32,45 @@ export const PulsatingButton: React.FC<PulsatingButtonProps> = ({
       className="flex justify-center items-center"
     >
       <button
-        className="relative block text-center cursor-pointer flex justify-center items-center rounded"
+        className="relative block text-center cursor-pointer flex justify-center items-center"
         style={{
           color: textColor,
           backgroundColor: backgroundColor,
           width: buttonWidth,
           height: buttonHeight,
+          borderRadius: '16px',
           ...pulseKeyframes,
         }}
       >
         <b>{text}</b>
-        <style jsx>{`
+        <style jsx
+          // style={{
+          //   borderRadius: '24px',
+          // }}
+        >{`
           @keyframes pulse {
             0% {
               box-shadow: 0 0 0 0 rgba(var(--tw-pulse-color), 0);
             }
             50% {
-              box-shadow: 0 0 0 10px rgba(var(--tw-pulse-color), 0.5);
+              box-shadow: 0 0 0 8px rgba(var(--tw-pulse-color), 0.5);
             }
             100% {
               box-shadow: 0 0 0 0 rgba(var(--tw-pulse-color), 0);
             }
+          }
+          button::before {
+            content: '';
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            width: 100%;
+            height: 100%;
+            border-radius: 24px;
+            background: inherit;
+            animation: inherit;
+            transform: translate(-50%, -50%);
+            z-index: -1;
           }
         `}</style>
       </button>
