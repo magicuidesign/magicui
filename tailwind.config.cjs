9f0146fb5a3668dcc946da97283e9602d3b3f972
mav <0xmav.eth@gmail.com>
2024-06-23 19:51:44 +0400

    custom animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -84,6 +84,8 @@ module.exports = {
         shine: "shine var(--duration) infinite linear",
         pulse: "pulse var(--duration) ease-out infinite",
         rainbow: "rainbow var(--speed, 2s) infinite linear",
+        "modal-fade-in": "modal-fade-in 500ms ease-out",
+        "modal-fade-out": "modal-fade-out 500ms ease-in",
       },
       keyframes: {
         "accordion-down": {
@@ -195,6 +197,14 @@ module.exports = {
         rainbow: {
           "0%": { "background-position": "0%" },
           "100%": { "background-position": "200%" },
+        },
+        "modal-fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        "modal-fade-out": {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
         },
       },
     },
