edad42d3e165059c0e608ec2801169eff758d827
Dillion Verma <hello@dillion.io>
2024-07-01 02:37:15 -0700

    fix: update missing @ symbol

--- registry/components/magicui/word-pull-up.tsx
+++ registry/components/magicui/word-pull-up.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { motion, Variants } from "framer-motion";
-import { cn } from "lib/utils";
 
 interface WordPullUpProps {
   words: string;
