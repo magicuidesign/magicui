c95ba96db773ad13b8591a52295bafff164db5bf
Dillion Verma <hello@dillion.io>
2024-06-30 21:18:20 -0700

    fix: remove unused types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,5 @@
 import { Icons } from "@/components/icons";
 
-declare global {
-  interface Window {
-    ApplePaySession: any;
-  }
-}
-
 export interface NavItem {
   title: string;
   href?: string;
@@ -26,20 +20,4 @@ export interface SidebarNavItem extends NavItemWithChildren {}
 export type DashboardConfig = {
   mainNav: MainNavItem[];
   sidebarNav: SidebarNavItem[];
-};
-
-export type SubscriptionPlan = {
-  name: string;
-  description: string;
-  stripePriceId: string;
-};
-
-export type UserSubscriptionPlan = SubscriptionPlan &
-  Pick<User, "stripeCustomerId" | "stripeSubscriptionId"> & {
-    stripeCurrentPeriodEnd: number;
-    isPro: boolean;
-  };
-
-export type UserPayments = Pick<User> & {
-  payments: Payment[];
 };
