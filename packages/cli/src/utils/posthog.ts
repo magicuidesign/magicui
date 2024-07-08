6898291cdb52ba62a9393eebef6a3c5b2886327c
Dillion Verma <hello@dillion.io>
2024-07-08 00:50:52 -0700

    fix: remove cli debugging

--- packages/cli/src/utils/posthog.ts
+++ packages/cli/src/utils/posthog.ts
@@ -4,4 +4,4 @@ export const posthog = new PostHog(process.env.POSTHOG_API_KEY!, {
   host: "https://us.i.posthog.com",
 });
 
-posthog.debug();
+if (process.env.NODE_ENV! === "development") posthog.debug();
