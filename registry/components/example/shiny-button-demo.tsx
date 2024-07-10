7d38ba63b732e0c8f0f3ee86ea36a14ffe803d88
abdrahmanES1 <e.abdrahman2002@gmail.com>
2024-07-10 09:56:31 +0100

    fix:  add the missing className props  #173

--- registry/components/example/shiny-button-demo.tsx
+++ registry/components/example/shiny-button-demo.tsx
@@ -1,5 +1,5 @@
 import ShinyButton from "@/components/magicui/shiny-button";
 
 export default function ShinyButtonDemo() {
-  return <ShinyButton text="Shiny Button" />;
+  return <ShinyButton text="Shiny Button" className="" />;
 }
