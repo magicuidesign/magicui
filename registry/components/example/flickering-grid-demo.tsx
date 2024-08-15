8ac24c5b4c72a74e5f941b99c06607cc14c94933
Dillion Verma <hello@dillion.io>
2024-08-15 20:47:57 +0200

    fix: update styles

--- registry/components/example/flickering-grid-demo.tsx
+++ registry/components/example/flickering-grid-demo.tsx
@@ -1,16 +1,17 @@
-import FlickeringGrid from "../magicui/flickering-grid";
+import FlickeringGrid from "@/components/magicui/flickering-grid";
 
 export default function FlickeringGridDemo() {
   return (
-    <div className="bg-[hsla(0,0%,5%,1)] relative overflow-hidden h-[500px] rounded-lg w-full">
+    <div className="relative h-[500px] rounded-lg w-full bg-background overflow-hidden border">
       <FlickeringGrid
         className="z-0 absolute inset-0 size-full"
         squareSize={4}
         gridGap={6}
-        color="#fff"
-        maxOpacity={0.05}
+        color="#6B7280"
+        maxOpacity={0.5}
         flickerChance={0.1}
-        height={1600}
+        height={800}
+        width={800}
       />
     </div>
   );
