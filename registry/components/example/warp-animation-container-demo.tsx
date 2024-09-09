ca05a31e06e523ff33d3cd037c5191737b8519c6
Nguyen Phuc Loc <nguyenphucloc@Locs-MacBook-Pro.local>
2024-09-09 20:27:05 +0700

    feat: add warp animation component

---
+++ registry/components/example/warp-animation-container-demo.tsx
@@ -0,0 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import WarpAnimationContainer from "@/components/magicui/warp-animation-container";
+
+export default function ExampleComponentDemo() {
+  return (
+    <div className="grid place-items-center">
+      <WarpAnimationContainer>
+        <Card className="w-80">
+          <CardContent className="flex flex-col gap-2 p-4">
+            <CardTitle>Congratulations on Your Promotion!</CardTitle>
+            <CardDescription>
+              Your hard work and dedication have paid off. We're thrilled to see
+              you take this next step in your career. Keep up the fantastic
+              work!
+            </CardDescription>
+          </CardContent>
+        </Card>
+      </WarpAnimationContainer>
+    </div>
+  );
+}
