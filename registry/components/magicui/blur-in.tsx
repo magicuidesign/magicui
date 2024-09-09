03d2db1fa24be27ca3161c837f5ead65532ebe86
Karanjot Singh <113842752+Karanxidhu@users.noreply.github.com>
2024-09-09 12:26:23 +0530

    Update blur-in.tsx (#271)
    className is now working in imported component

--- registry/components/magicui/blur-in.tsx
+++ registry/components/magicui/blur-in.tsx
@@ -27,8 +27,8 @@ const BlurIn = ({ word, className, variant, duration = 1 }: BlurIntProps) => {
       transition={{ duration }}
       variants={combinedVariants}
       className={cn(
-        className,
         "font-display text-center text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]",
+        className,
       )}
     >
       {word}
