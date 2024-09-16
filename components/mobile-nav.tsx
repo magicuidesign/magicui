ef20a66409066a3ea8e53c5ffe9163d3ef06e6cc
Dillion Verma <hello@dillion.io>
2024-09-16 03:24:23 -0400

    fix: update sidebar icon

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/navigation";
-import { SidebarOpen } from "lucide-react";
 import posthog from "posthog-js";
 
 import { docsConfig } from "@/config/docs";
@@ -24,7 +23,35 @@ export function MobileNav() {
           variant="ghost"
           className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
         >
-          <SidebarOpen className="size-6" />
+          <svg
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="size-5"
+          >
+            <path
+              d="M3 5H11"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+            <path
+              d="M3 12H16"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+            <path
+              d="M3 19H21"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+          </svg>
           <span className="sr-only">Toggle Menu</span>
         </Button>
       </SheetTrigger>
