c779f74e649b81b4b032bc68f64a286b2442e701
Singgih Adi Nugroho <9331718+snggh@users.noreply.github.com>
2024-09-24 13:28:20 +0700

    feat: add number-ticker to accept decimal places (#308)

--- registry/components/magicui/number-ticker.tsx
+++ registry/components/magicui/number-ticker.tsx
@@ -10,11 +10,13 @@ export default function NumberTicker({
   direction = "up",
   delay = 0,
   className,
+  decimalPlaces = 0,
 }: {
   value: number;
   direction?: "up" | "down";
   className?: string;
   delay?: number; // delay in s
+  decimalPlaces?: number;
 }) {
   const ref = useRef<HTMLSpanElement>(null);
   const motionValue = useMotionValue(direction === "down" ? value : 0);
@@ -35,12 +37,13 @@ export default function NumberTicker({
     () =>
       springValue.on("change", (latest) => {
         if (ref.current) {
-          ref.current.textContent = Intl.NumberFormat("en-US").format(
-            Number(latest.toFixed(0)),
-          );
+          ref.current.textContent = Intl.NumberFormat("en-US", {
+            minimumFractionDigits: decimalPlaces,
+            maximumFractionDigits: decimalPlaces,
+          }).format(Number(latest.toFixed(decimalPlaces)));
         }
       }),
-    [springValue],
+    [springValue, decimalPlaces],
   );
 
   return (
