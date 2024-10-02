b2dec1452173449caeee0889bb4523322a9924b0
Rekash <benmehal.joris@gmail.com>
2024-10-02 15:04:19 +0200

    fix: Refactor TextReveal component to use MotionValue for progress and update Tailwind classes

--- registry/default/magicui/text-reveal.tsx
+++ registry/default/magicui/text-reveal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FC, ReactNode, useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 
@@ -25,15 +25,13 @@ export const TextRevealByWord: FC<TextRevealByWordProps> = ({
     <div ref={targetRef} className={cn("relative z-0 h-[200vh]", className)}>
       <div
         className={
-          "sticky top-0 mx-auto flex h-[50%] max-w-4xl items-center bg-transparent px-[1rem] py-[5rem]"
-        }
-      >
+          "sticky top-0 mx-auto flex h-[1/2] max-w-4xl items-center bg-transparent px-4 py-20"
+        }>
         <p
           ref={targetRef}
           className={
             "flex flex-wrap p-5 text-2xl font-bold text-black/20 dark:text-white/20 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl"
-          }
-        >
+          }>
           {words.map((word, i) => {
             const start = i / words.length;
             const end = start + 1 / words.length;
@@ -51,7 +49,7 @@ export const TextRevealByWord: FC<TextRevealByWordProps> = ({
 
 interface WordProps {
   children: ReactNode;
-  progress: any;
+  progress: MotionValue<number>;
   range: [number, number];
 }
 
@@ -62,8 +60,7 @@ const Word: FC<WordProps> = ({ children, progress, range }) => {
       <span className={"absolute opacity-30"}>{children}</span>
       <motion.span
         style={{ opacity: opacity }}
-        className={"text-black dark:text-white"}
-      >
+        className={"text-black dark:text-white"}>
         {children}
       </motion.span>
     </span>
