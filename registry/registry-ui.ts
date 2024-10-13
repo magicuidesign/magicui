c06ab53526ad6e4b492a68298f921eb910458ef7
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2024-10-13 18:59:49 +0900

    fix: missing tailwind prop on meteors

--- registry/registry-ui.ts
+++ registry/registry-ui.ts
@@ -34,6 +34,30 @@ export const ui: Registry = [
     name: "meteors",
     type: "registry:ui",
     files: ["magicui/meteors.tsx"],
+    tailwind: {
+      config: {
+        theme: {
+          extend: {
+            animation: {
+				      meteor: "meteor 5s linear infinite",
+			      },
+			      keyframes: {
+				      meteor: {
+					      "0%": {
+						      transform: "rotate(215deg) translateX(0)",
+						      opacity: "1",
+					      },
+					      "70%": { opacity: "1" },
+					      "100%": {
+						      transform: "rotate(215deg) translateX(-500px)",
+						      opacity: "0",
+					      },
+				      },
+			      },
+          },
+        },
+      },
+    },
   },
   {
     name: "grid-pattern",
