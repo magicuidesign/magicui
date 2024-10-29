b22b95e226c04e32f96ac304d9e612d5d5bb5017
Chuang Yu <cyu9960@gmail.com>
2024-10-29 15:36:10 +0800

    refactor: remove unnecessary 'grid' class from ShineBorder component

--- registry/default/magicui/shine-border.tsx
+++ registry/default/magicui/shine-border.tsx
@@ -39,7 +39,7 @@ export default function ShineBorder({
         } as React.CSSProperties
       }
       className={cn(
-        "relative grid min-h-[60px] w-fit min-w-[300px] place-items-center rounded-[--border-radius] bg-white p-3 text-black dark:bg-black dark:text-white",
+        "relative min-h-[60px] w-fit min-w-[300px] place-items-center rounded-[--border-radius] bg-white p-3 text-black dark:bg-black dark:text-white",
         className,
       )}
     >
