eae001c7107dfbc98a2ec3c7a2fd964d055ed7c0
Irfan Maulana <mazipanneh@gmail.com>
2024-11-12 11:38:09 +0700

    core: update typings in WordFadeIn and TextRevealByWord (#392)
    * chore: typing in text-reveal and word-fade-in
    
    chore: bring back old format
    
    chore: revert settings.json
    
    * feat: added the correct import from framer-motion
    
    ---------
    
    Co-authored-by: Arghya Das <arghyadasproject@gmail.com>

--- registry/default/magicui/text-reveal.tsx
+++ registry/default/magicui/text-reveal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FC, ReactNode, useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 
@@ -51,7 +51,7 @@ export const TextRevealByWord: FC<TextRevealByWordProps> = ({
 
 interface WordProps {
   children: ReactNode;
-  progress: any;
+  progress: MotionValue<number>;
   range: [number, number];
 }
 
