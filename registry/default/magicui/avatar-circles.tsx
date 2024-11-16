1a0f4a90965e3eec0cb981e7ae8290cea1f115d3
Arghya Das <arghyadasproject@gmail.com>
2024-11-16 23:19:56 +0530

    fix: lint

--- registry/default/magicui/avatar-circles.tsx
+++ registry/default/magicui/avatar-circles.tsx
@@ -24,20 +24,20 @@ const AvatarCircles = ({
     <div className={cn("z-10 flex -space-x-4 rtl:space-x-reverse", className)}>
       {avatarUrls.map((url, index) => (
         <a
-        key={index}
-        href={url.profileUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
           key={index}
-          className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
-          src={url.imageUrl}
-          width={40}
-          height={40}
-          alt={`Avatar ${index + 1}`}
-        />
-      </a>
+          href={url.profileUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            key={index}
+            className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+            src={url.imageUrl}
+            width={40}
+            height={40}
+            alt={`Avatar ${index + 1}`}
+          />
+        </a>
       ))}
       {numPeople && (
         <a
