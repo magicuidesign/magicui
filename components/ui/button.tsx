67b549025cfe1e9ae31491bc50f2aa1addb245d0
Ozan Tekin <atekin.mdbf22@iste.edu.tr>
2024-12-12 19:01:52 +0300

    Fix #331: Add SheetClose and update usage to close mobile menu (#332)
    * fix(mobile-nav): add  and update usage to close menu after item click
    
    * fix: lint fix
    
    ---------
    
    Co-authored-by: Arghya Das <arghyadasproject@gmail.com>

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -35,7 +35,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 export interface ButtonProps
@@ -54,7 +54,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  }
+  },
 );
 Button.displayName = "Button";
 
