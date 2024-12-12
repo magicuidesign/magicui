d1fdb64b0155ed3676daccaf78024540f1df44b6
LuggaPugga <45566784+LuggaPugga@users.noreply.github.com>
2024-12-12 18:02:30 +0100

    fix: put the component file in right place (#240)
    Co-authored-by: Arghya Das <arghyadasproject@gmail.com>

--- registry/default/magicui/avatar-circles.tsx
+++ registry/default/magicui/avatar-circles.tsx
@@ -39,10 +39,10 @@ const AvatarCircles = ({
           />
         </a>
       ))}
-      {numPeople && (
+      {(numPeople ?? 0) > 0 && (
         <a
           className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black"
-          href="#"
+          href=""
         >
           +{numPeople}
         </a>
