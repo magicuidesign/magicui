b084791e8113463587b720092ef9f221b3b3fc5f
Anshuman Singh Rathore <anshumanrathore328@gmail.com>
2024-12-19 04:32:55 +0530

    feat: add npmCopyButton functionality in vite astro and remix (#387)
    * feat: add npmcopybutton functionality in vite astro and remix
    
    * fix: lint
    
    ---------
    
    Co-authored-by: anshumanRathore-coder <anshumantathore328@gmail.com>
    Co-authored-by: Dillion Verma <hello@dillion.io>

--- lib/rehype-npm-command.ts
+++ lib/rehype-npm-command.ts
@@ -61,6 +61,23 @@ export function rehypeNpmCommand() {
           "bunx --bun",
         );
       }
+      // npm create.
+      if (node.properties?.["__rawString__"]?.startsWith("npm create")) {
+        const npmCommand = node.properties?.["__rawString__"];
+        node.properties["__npmCommand__"] = npmCommand;
+        node.properties["__yarnCommand__"] = npmCommand.replace(
+          "npm create",
+          "yarn create",
+        );
+        node.properties["__pnpmCommand__"] = npmCommand.replace(
+          "npm create",
+          "pnpm create",
+        );
+        node.properties["__bunCommand__"] = npmCommand.replace(
+          "npm create",
+          "bun create",
+        );
+      }
     });
   };
 }
