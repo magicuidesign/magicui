3a7db902c54a0a965970b9fd73275b4b1b611c10
Dillion Verma <hello@dillion.io>
2024-12-18 16:44:48 -0500

    fix: update prettier settings

--- .prettier.config.cjs
+++ .prettier.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   semi: false,
   singleQuote: false,
   tabWidth: 2,
-  trailingComma: "es5",
+  trailingComma: "all",
   importOrder: [
     "^(react/(.*)$)|^(react$)",
     "^(next/(.*)$)|^(next$)",
