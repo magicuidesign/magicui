bc7808d78895a3c2c35bbe21de8c77f32eea67a1
Arghya Das <arghyadasproject@gmail.com>
2024-12-19 15:56:50 +0530

    fix: adding use-client to the dock-demos

--- registry/default/example/dock-demo-2.tsx
+++ registry/default/example/dock-demo-2.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 import { Dock, DockIcon } from "@/registry/default/magicui/dock";
--- registry/default/example/dock-demo.tsx
+++ registry/default/example/dock-demo.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { CalendarIcon, HomeIcon, MailIcon, PencilIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
