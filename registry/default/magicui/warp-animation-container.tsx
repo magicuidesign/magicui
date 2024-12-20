790069e902a5bca953c97b1ee2081e7117eff10b
Dillion Verma <hello@dillion.io>
2024-12-20 13:40:54 -0500

    fix: update code

--- registry/default/magicui/warp-animation-container.tsx
+++ registry/default/magicui/warp-animation-container.tsx
@@ -80,13 +80,7 @@ const WarpAnimationContainer: React.FC<WarpAnimationContainerProps> = ({
   const leftBeams = useMemo(() => generateBeams(), [generateBeams]);
 
   return (
-    <div
-      className={cn(
-        "relative grid place-items-center rounded border p-20",
-        className,
-      )}
-      {...props}
-    >
+    <div className={cn("relative rounded border p-20", className)} {...props}>
       <div
         style={
           {
