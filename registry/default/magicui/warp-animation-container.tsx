252b7aa2d71bd40d591a9230b9518fab50cbeabe
Dillion Verma <hello@dillion.io>
2024-12-20 13:38:44 -0500

    fix: update styles

--- registry/default/magicui/warp-animation-container.tsx
+++ registry/default/magicui/warp-animation-container.tsx
@@ -1,13 +1,10 @@
-// registry/default/magicui/warp-animation-container.tsx
-import React, { useCallback, useMemo } from "react";
-import clsx from "clsx";
-import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import React, { HTMLAttributes, useCallback, useMemo } from "react";
 
-interface WarpAnimationContainerProps {
+interface WarpAnimationContainerProps extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   perspective?: number;
-  className?: string;
   beamsPerSide?: number;
   beamSize?: number;
   beamDelayMax?: number;
@@ -32,7 +29,7 @@ const Beam = ({
 
   return (
     <motion.div
-      className={clsx(`absolute top-0`)}
+      className={`absolute top-0`}
       style={{
         left: x,
         width,
@@ -60,14 +57,9 @@ const WarpAnimationContainer: React.FC<WarpAnimationContainerProps> = ({
   beamDelayMax = 3,
   beamDelayMin = 0,
   beamDuration = 3,
-  gridColor = "#f0f0f0",
+  gridColor = "hsl(var(--border))",
+  ...props
 }) => {
-  const sideStyle: React.CSSProperties = {
-    position: "absolute",
-    transformStyle: "preserve-3d",
-    containerType: "inline-size",
-  };
-
   const generateBeams = useCallback(() => {
     const beams = [];
     const cellsPerSide = Math.floor(100 / beamSize);
@@ -89,36 +81,39 @@ const WarpAnimationContainer: React.FC<WarpAnimationContainerProps> = ({
 
   return (
     <div
-      className={clsx(
-        "relative grid place-items-center border rounded p-20",
-        className
+      className={cn(
+        "relative grid place-items-center rounded border p-20",
+        className,
       )}
+      {...props}
     >
       <div
-        className={cn(
-          "pointer-events-none absolute left-0 top-0 size-full overflow-hidden",
-          `[perspective:${perspective}px]`,
-          "[container-type:size]",
-          "[transform-style:preserve-3d]",
-          "[clip-path:inset(0)]"
-        )}
-        style={{
-          perspective: `${perspective}px`,
-        }}
+        style={
+          {
+            "--perspective": `${perspective}px`,
+            "--grid-color": gridColor,
+            "--beam-size": `${beamSize}%`,
+          } as React.CSSProperties
+        }
+        className={
+          "pointer-events-none absolute left-0 top-0 size-full overflow-hidden [clip-path:inset(0)] [container-type:size] [perspective:var(--perspective)] [transform-style:preserve-3d]"
+        }
       >
         <div
           style={{
-            ...sideStyle,
+            position: "absolute",
+            transformStyle: "preserve-3d",
+            containerType: "inline-size",
             width: "100cqi",
             height: "100cqmax",
             transformOrigin: "50% 0%",
             transform: "rotateX(-90deg)",
-            backgroundSize: `${beamSize}% ${beamSize}%`,
+            backgroundSize: `var(--beam-size) var(--beam-size)`,
             background: `
-            linear-gradient(${gridColor} 0 1px, transparent 1px ${beamSize}%) 50% -0.5px /
-            ${beamSize}% ${beamSize}%,
-            linear-gradient(90deg, ${gridColor} 0 1px, transparent 1px ${beamSize}%)
-            50% 50% / ${beamSize}% ${beamSize}%
+            linear-gradient(var(--grid-color) 0 1px, transparent 1px var(--beam-size)) 50% -0.5px /
+            var(--beam-size) var(--beam-size),
+            linear-gradient(90deg, var(--grid-color) 0 1px, transparent 1px var(--beam-size))
+            50% 50% / var(--beam-size) var(--beam-size)
           `,
           }}
         >
@@ -134,18 +129,20 @@ const WarpAnimationContainer: React.FC<WarpAnimationContainerProps> = ({
         </div>
         <div
           style={{
-            ...sideStyle,
+            position: "absolute",
+            transformStyle: "preserve-3d",
+            containerType: "inline-size",
             width: "100cqi",
             height: "100cqmax",
             top: "100%",
             transformOrigin: "50% 0%",
             transform: "rotateX(-90deg)",
-            backgroundSize: `${beamSize}% ${beamSize}%`,
+            backgroundSize: `var(--beam-size) var(--beam-size)`,
             background: `
-            linear-gradient(${gridColor} 0 1px, transparent 1px ${beamSize}%) 50% -0.5px /
-            ${beamSize}% ${beamSize}%,
-            linear-gradient(90deg, ${gridColor} 0 1px, transparent 1px ${beamSize}%)
-            50% 50% / ${beamSize}% ${beamSize}%
+            linear-gradient(var(--grid-color) 0 1px, transparent 1px var(--beam-size)) 50% -0.5px /
+            var(--beam-size) var(--beam-size),
+            linear-gradient(90deg, var(--grid-color) 0 1px, transparent 1px var(--beam-size))
+            50% 50% / var(--beam-size) var(--beam-size)
           `,
           }}
         >
@@ -161,19 +158,21 @@ const WarpAnimationContainer: React.FC<WarpAnimationContainerProps> = ({
         </div>
         <div
           style={{
-            ...sideStyle,
+            position: "absolute",
+            transformStyle: "preserve-3d",
+            containerType: "inline-size",
             width: "100cqh",
             height: "100cqmax",
             top: 0,
             left: 0,
             transformOrigin: "0% 0%",
             transform: "rotate(90deg) rotateX(-90deg)",
-            backgroundSize: `${beamSize}% ${beamSize}%`,
+            backgroundSize: `var(--beam-size) var(--beam-size)`,
             background: `
-            linear-gradient(${gridColor} 0 1px, transparent 1px ${beamSize}%) 50% -0.5px /
-            ${beamSize}% ${beamSize}%,
-            linear-gradient(90deg, ${gridColor} 0 1px, transparent 1px ${beamSize}%)
-            50% 50% / ${beamSize}% ${beamSize}%
+            linear-gradient(var(--grid-color) 0 1px, transparent 1px var(--beam-size)) 50% -0.5px /
+            var(--beam-size) var(--beam-size),
+            linear-gradient(90deg, var(--grid-color) 0 1px, transparent 1px var(--beam-size))
+            50% 50% / var(--beam-size) var(--beam-size)
           `,
           }}
         >
@@ -189,19 +188,21 @@ const WarpAnimationContainer: React.FC<WarpAnimationContainerProps> = ({
         </div>
         <div
           style={{
-            ...sideStyle,
+            position: "absolute",
+            transformStyle: "preserve-3d",
+            containerType: "inline-size",
             width: "100cqh",
             height: "100cqmax",
             top: 0,
             right: 0,
             transformOrigin: "100% 0%",
             transform: "rotate(-90deg) rotateX(-90deg)",
-            backgroundSize: `${beamSize}% ${beamSize}%`,
+            backgroundSize: `var(--beam-size) var(--beam-size)`,
             background: `
-            linear-gradient(${gridColor} 0 1px, transparent 1px ${beamSize}%) 50% -0.5px /
-            ${beamSize}% ${beamSize}%,
-            linear-gradient(90deg, ${gridColor} 0 1px, transparent 1px ${beamSize}%)
-            50% 50% / ${beamSize}% ${beamSize}%
+            linear-gradient(var(--grid-color) 0 1px, transparent 1px var(--beam-size)) 50% -0.5px /
+            var(--beam-size) var(--beam-size),
+            linear-gradient(90deg, var(--grid-color) 0 1px, transparent 1px var(--beam-size))
+            50% 50% / var(--beam-size) var(--beam-size)
           `,
           }}
         >
