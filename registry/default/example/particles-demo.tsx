b9cf05195e9962cb4d174ed68986d6c2e158e3a8
Arghya Das <arghyadasproject@gmail.com>
2024-12-27 23:46:18 +0530

    fix: fixing the flickering particles issue on resize (#471)
    * fix: fixing the flickering particles issue on resize
    
    * fix: update zindex
    
    ---------
    
    Co-authored-by: Dillion Verma <hello@dillion.io>

--- registry/default/example/particles-demo.tsx
+++ registry/default/example/particles-demo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 import Particles from "@/registry/default/magicui/particles";
 
@@ -15,11 +15,12 @@ export default function ParticlesDemo() {
 
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+      <span className="pointer-events-none z-10 whitespace-pre-wrap text-center text-8xl font-semibold leading-none">
         Particles
       </span>
+      q
       <Particles
-        className="absolute inset-0"
+        className="absolute inset-0 z-0"
         quantity={100}
         ease={80}
         color={color}
