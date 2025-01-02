6ad2bb224e126e9a6108bdadb62b2ff6ebc9fd8a
Dillion Verma <hello@dillion.io>
2025-01-01 22:32:21 -0500

    fix: style fix

--- registry/default/example/text-animate-demo.tsx
+++ registry/default/example/text-animate-demo.tsx
@@ -3,7 +3,7 @@ import { TextAnimate } from "@/registry/default/magicui/text-animate";
 export default function TextAnimateDemo() {
   return (
     <TextAnimate animation="blurInUp" by="character">
-      Blur fade by character
+      Blur in by character
     </TextAnimate>
   );
 }
