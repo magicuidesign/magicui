4139f831927db9124618bec8eb9a99e9a9e8c369
Dillion Verma <hello@dillion.io>
2025-01-13 17:13:21 -0500

    fix: set capture pageview to false

--- components/posthog-provider.tsx
+++ components/posthog-provider.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY!, {
     api_host: "https://app.posthog.com",
+    capture_pageview: false,
     session_recording: {
       maskAllInputs: false,
     },
