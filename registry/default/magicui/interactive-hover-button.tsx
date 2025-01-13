d80222c2d38315d3700c577910377da61a2dc883
moonrailgun <moonrailgun@gmail.com>
2025-01-13 22:09:23 +0800

    fix

--- registry/default/magicui/interactive-hover-button.tsx
+++ registry/default/magicui/interactive-hover-button.tsx
@@ -20,7 +20,7 @@ const InteractiveHoverButton = React.forwardRef<
       )}
       {...props}
     >
-      <span className="inline-block translate-x-1 transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
+      <span className="text-primary inline-block translate-x-1 transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
         {text}
       </span>
       <div className="absolute top-0 z-10 flex h-full w-full translate-x-12 items-center justify-center gap-2 text-primary-foreground opacity-0 transition-all duration-300 group-hover:-translate-x-1 group-hover:opacity-100">
