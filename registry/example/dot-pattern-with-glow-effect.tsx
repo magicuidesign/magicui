69112816d4c8921f20900281405cd663684b5758
Isaac Hermel <isaachermel@gmail.com>
2025-02-06 21:39:44 -0300

    style: fix format on dot-pattern demo

--- registry/example/dot-pattern-with-glow-effect.tsx
+++ registry/example/dot-pattern-with-glow-effect.tsx
@@ -12,7 +12,7 @@ export default function DotPatternWithGlowEffectDemo() {
       <DotPattern
         glow={true}
         className={cn(
-          "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]"
+          "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",
         )}
       />
     </div>
