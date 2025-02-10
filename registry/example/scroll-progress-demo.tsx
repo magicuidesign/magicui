cb37697d0c42511dfe58fdef34a2be1384fa3894
Dillion Verma <hello@dillion.io>
2025-02-10 16:34:11 -0500

    fix: update more styles

--- registry/example/scroll-progress-demo.tsx
+++ registry/example/scroll-progress-demo.tsx
@@ -2,17 +2,11 @@ import { ScrollProgress } from "@/registry/magicui/scroll-progress";
 
 export default function ScrollProgressDemo() {
   return (
-    <div className="z-10 rounded-lg border border-gray-200 bg-white p-4">
+    <div className="z-10 rounded-lg p-4">
       <ScrollProgress className="top-[65px]" />
       <h2 className="pb-4 font-bold">
         Note: The scroll progress is shown below the navbar of the page.
       </h2>
-      <p className="pb-4">
-        Magic UI is a collection of re-usable components that you can copy and
-        paste into your web apps. It primarily features components, blocks, and
-        templates geared towards creating landing pages and user-facing
-        marketing materials.
-      </p>
     </div>
   );
 }
