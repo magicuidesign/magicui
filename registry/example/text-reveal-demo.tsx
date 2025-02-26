c4cf068c50106935ca7af0eccb680dca5d935f46
Dillion Verma <hello@dillion.io>
2025-02-26 14:10:55 -0500

    dillion/text reveal api update (#573)
    * fix: update text reveal api
    
    * fix: update text reveal api

--- registry/example/text-reveal-demo.tsx
+++ registry/example/text-reveal-demo.tsx
@@ -1,9 +1,5 @@
 import { TextReveal } from "@/registry/magicui/text-reveal";
 
 export default function TextRevealDemo() {
-  return (
-    <div className="z-10 flex min-h-64 items-center justify-center rounded-lg border bg-white dark:bg-black">
-      <TextReveal text="Magic UI will change the way you design." />
-    </div>
-  );
+  return <TextReveal>Magic UI will change the way you design.</TextReveal>;
 }
