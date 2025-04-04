ce04e654d95569bb8b79e00258f169308f2f6b5f
Dillion Verma <hello@dillion.io>
2025-04-04 12:01:33 -0400

    fix: update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,53 @@
-import { clsx, type ClassValue } from "clsx";
+import { env } from "@/env.mjs";
+import clsx, { ClassValue } from "clsx";
 import { Metadata } from "next";
-import { env } from "process";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function humanize(name: string): string {
+  return name
+    .replace(/-/g, " ")
+    .replace(/([A-Z])/g, " $1")
+    .trim()
+    .split(/\s+/)
+    .map((word) => word[0].toUpperCase() + word.substring(1).toLowerCase())
+    .join(" ");
+}
+
+export const truncate = (str: string | null, length: number) => {
+  if (!str || str.length <= length) return str;
+  return `${str.slice(0, length - 3)}...`;
+};
+
+export const fetcher = (...args: Parameters<typeof fetch>) =>
+  fetch(...args).then((res) => res.json());
+
+/**
+ * Capitalizes first letters of words in string.
+ * @param {string} str String to be modified
+ * @param {boolean=false} lower Whether all other letters should be lowercased
+ * @return {string}
+ * @see https://stackoverflow.com/questions/2332811/capitalize-words-in-string/7592235#7592235
+ * @usage
+ *   capitalize('fix this string');     // -> 'Fix This String'
+ *   capitalize('javaSCrIPT');          // -> 'JavaSCrIPT'
+ *   capitalize('javaSCrIPT', true);    // -> 'Javascript'
+ */
+export const capitalize = (str: string, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+    match.toUpperCase(),
+  );
+
+export function formatDate(input: string | number): string {
+  const date = new Date(input);
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
 }
 
 export function absoluteUrl(path: string) {
@@ -28,7 +71,7 @@ export function constructMetadata({
     keywords: [
       "React",
       "Tailwind CSS",
-      "Framer Motion",
+      "Motion",
       "Landing Page",
       "Components",
       "Next.js",
@@ -53,7 +96,7 @@ export function constructMetadata({
       creator: "@dillionverma",
     },
     icons: "/favicon.ico",
-    metadataBase: new URL("https://pro.magicui.design"),
+    metadataBase: new URL("https://magicui.design"),
     authors: [
       {
         name: "dillionverma",
@@ -61,34 +104,6 @@ export function constructMetadata({
       },
     ],
     creator: "dillionverma",
-    // themeColor: [
-    //   { media: "(prefers-color-scheme: light)", color: "white" },
-    //   { media: "(prefers-color-scheme: dark)", color: "black" },
-    // ],
     ...props,
   };
 }
-
-export const getURL = (path: string = "") => {
-  // Check if NEXT_PUBLIC_APP_URL is set and non-empty. Set this to your site URL in production env.
-  let url =
-    process?.env?.NEXT_PUBLIC_APP_URL &&
-    process.env.NEXT_PUBLIC_APP_URL.trim() !== ""
-      ? process.env.NEXT_PUBLIC_APP_URL
-      : // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-          process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ""
-        ? process.env.NEXT_PUBLIC_VERCEL_URL
-        : // If neither is set, default to localhost for local development.
-          "http://localhost:3003/";
-
-  // Trim the URL and remove trailing slash if exists.
-  url = url.replace(/\/+$/, "");
-  // Make sure to include `https://` when not localhost.
-  url = url.includes("http") ? url : `https://${url}`;
-  // Ensure path starts without a slash to avoid double slashes in the final URL.
-  path = path.replace(/^\/+/, "");
-
-  // Concatenate the URL and the path.
-  return path ? `${url}/${path}` : url;
-};
