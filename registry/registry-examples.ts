ccd207fd829d74cf9a72a6c68406653830280908
Syeda Hoorain Ali <syedahoorainali8@gmail.com>
2025-04-20 06:50:48 +0500

    feat: added glow component

--- registry/registry-examples.ts
+++ registry/registry-examples.ts
@@ -1710,4 +1710,19 @@ export const examples: Registry["items"] = [
       },
     ],
   },
+  {
+    name: "glow-demo",
+    type: "registry:example",
+    title: "Glow Demo",
+    description:
+      "Example showing a beautiful glow effect that appears when hovering over elements.",
+    registryDependencies: ["https://magicui.design/r/glow"],
+    files: [
+      {
+        path: "registry/example/glow-demo.tsx",
+        type: "registry:example",
+        // target: "components/glow-demo.tsx",
+      },
+    ],
+  },
 ];
