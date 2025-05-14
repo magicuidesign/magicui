46b0181eae58e864ee1285aea0adaa2280cf095b
Arghya Das <arghyadasproject@gmail.com>
2025-05-14 18:05:28 +0530

    feat: fixed the dock build issue

--- registry/magicui/dock.tsx
+++ registry/magicui/dock.tsx
@@ -47,7 +47,10 @@ const Dock = React.forwardRef<HTMLDivElement, DockProps>(
 
     const renderChildren = () => {
       return React.Children.map(children, (child) => {
-        if (React.isValidElement(child) && child.type === DockIcon) {
+        if (
+          React.isValidElement<DockIconProps>(child) &&
+          child.type === DockIcon
+        ) {
           return React.cloneElement(child, {
             ...child.props,
             mouseX: mouseX,
