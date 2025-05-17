cc912fcf21be550cd9a178c40940db26dc2fd9e4
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2025-05-17 18:45:24 +0530

    add type defintion for strokeDasharray and add repeatDelay to transition to prevent TS unused error

--- registry/magicui/animated-grid-pattern.tsx
+++ registry/magicui/animated-grid-pattern.tsx
@@ -17,7 +17,7 @@ export interface AnimatedGridPatternProps
   height?: number;
   x?: number;
   y?: number;
-  strokeDasharray?: any;
+  strokeDasharray?: number;
   numSquares?: number;
   maxOpacity?: number;
   duration?: number;
@@ -81,7 +81,7 @@ export function AnimatedGridPattern({
   // Resize observer to update container dimensions
   useEffect(() => {
     const resizeObserver = new ResizeObserver((entries) => {
-      for (let entry of entries) {
+      for (const entry of entries) {
         setDimensions({
           width: entry.contentRect.width,
           height: entry.contentRect.height,
@@ -136,6 +136,7 @@ export function AnimatedGridPattern({
               duration,
               repeat: 1,
               delay: index * 0.1,
+              repeatDelay,
               repeatType: "reverse",
             }}
             onAnimationComplete={() => updateSquarePosition(id)}
