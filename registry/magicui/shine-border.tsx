cfb3e6ec7a24cda92d096504b420039dc063855f
nuu-maan <n82238895@gmail.com>
2025-06-30 19:59:12 +0530

    fix: restore shine-border animation and add disableMask prop

--- registry/magicui/shine-border.tsx
+++ registry/magicui/shine-border.tsx
@@ -20,6 +20,10 @@ interface ShineBorderProps extends React.HTMLAttributes<HTMLDivElement> {
    * @default "#000000"
    */
   shineColor?: string | string[];
+  /**
+   * Disable masking for debugging
+   */
+  disableMask?: boolean;
 }
 
 /**
@@ -33,6 +37,7 @@ export function ShineBorder({
   shineColor = "#000000",
   className,
   style,
+  disableMask = false,
   ...props
 }: ShineBorderProps) {
   return (
@@ -45,10 +50,14 @@ export function ShineBorder({
             Array.isArray(shineColor) ? shineColor.join(",") : shineColor
           },transparent,transparent)`,
           backgroundSize: "300% 300%",
-          mask: `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
-          WebkitMask: `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
-          WebkitMaskComposite: "xor",
-          maskComposite: "exclude",
+          ...(disableMask
+            ? {}
+            : {
+                mask: `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
+                WebkitMask: `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
+                WebkitMaskComposite: "xor",
+                maskComposite: "exclude",
+              }),
           padding: "var(--border-width)",
           ...style,
         } as React.CSSProperties
