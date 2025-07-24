c99ec47a76c5e779ac2d4fe5e7c995a8b2454adb
LuggaPugga <45566784+LuggaPugga@users.noreply.github.com>
2025-07-24 18:29:25 +0200

    fix: add title attribute to iframe in HeroVideoDialog component

--- registry/magicui/hero-video-dialog.tsx
+++ registry/magicui/hero-video-dialog.tsx
@@ -127,6 +127,7 @@ export default function HeroVideoDialog({
               <div className="relative isolate z-[1] size-full overflow-hidden rounded-2xl border-2 border-white">
                 <iframe
                   src={videoSrc}
+                  title="Hero Video Dialog"
                   className="size-full rounded-2xl"
                   allowFullScreen
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
