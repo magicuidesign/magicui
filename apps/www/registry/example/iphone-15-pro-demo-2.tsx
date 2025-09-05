e6c2ecf8ef15f13118fc10d2b96955dde1fd41c0
Yeom-JinHo <pwlsghq@naver.com>
2025-09-05 22:51:35 +0900

    docs:(Iphone15Pro): update example

--- apps/www/registry/example/iphone-15-pro-demo-2.tsx
+++ apps/www/registry/example/iphone-15-pro-demo-2.tsx
@@ -2,11 +2,8 @@ import { Iphone15Pro } from "@/registry/magicui/iphone-15-pro";
 
 export default function Iphone15ProDemo() {
   return (
-    <div className="relative">
-      <Iphone15Pro
-        className="size-full"
-        src="https://via.placeholder.com/430x880"
-      />
+    <div className="w-[434px]">
+      <Iphone15Pro src="https://via.placeholder.com/430x880" />
     </div>
   );
 }
--- apps/www/registry/example/iphone-15-pro-demo.tsx
+++ apps/www/registry/example/iphone-15-pro-demo.tsx
@@ -2,8 +2,8 @@ import { Iphone15Pro } from "@/registry/magicui/iphone-15-pro";
 
 export default function Iphone15ProDemo() {
   return (
-    <div className="relative">
-      <Iphone15Pro className="size-full" />
+    <div className="w-[434px]">
+      <Iphone15Pro />
     </div>
   );
 }
