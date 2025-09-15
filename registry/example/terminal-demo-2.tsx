ab072c403034c53e3b974dd21aae8498da6c5296
Dillion Verma <hello@dillion.io>
2025-09-15 00:19:08 -0400

    dillion/terminal example update (#794)
    * fix: update terminal demo
    
    * fix: update

---
+++ registry/example/terminal-demo-2.tsx
@@ -0,0 +1,25 @@
+import {
+  AnimatedSpan,
+  Terminal,
+  TypingAnimation,
+} from "@/registry/magicui/terminal";
+
+export default function TerminalDemo2() {
+  return (
+    <Terminal>
+      <TypingAnimation delay={0}>$ ls</TypingAnimation>
+
+      <AnimatedSpan delay={800} className="text-blue-500">
+        Documents Downloads Pictures
+      </AnimatedSpan>
+
+      <TypingAnimation delay={1600}>$ cd Documents</TypingAnimation>
+
+      <TypingAnimation delay={2400}>$ pwd</TypingAnimation>
+
+      <AnimatedSpan delay={3200} className="text-green-500">
+        /home/user/Documents
+      </AnimatedSpan>
+    </Terminal>
+  );
+}
