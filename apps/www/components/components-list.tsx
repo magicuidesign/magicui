b2f958849ae0281851050c0fd4004eac4de5429e
Dillion Verma <hello@dillion.io>
2025-09-16 22:52:52 -0400

    fix: small fix

--- apps/www/components/components-list.tsx
+++ apps/www/components/components-list.tsx
@@ -16,7 +16,7 @@ export function ComponentsList() {
   );
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-x-8 lg:gap-x-16 lg:gap-y-6 xl:gap-x-20">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-x-8 lg:gap-x-8 lg:gap-y-6">
       {list.map((component) => (
         <Link
           key={component.$id}
