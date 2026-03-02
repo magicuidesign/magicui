b533e64ee32db187a156f138be83d0c33dbce716
Yeom-JinHo <pwlsghq@naver.com>
2026-03-02 10:33:54 +0900

    chore(registry): mark MotionProps as type imports

--- apps/www/registry/magicui/text-animate.tsx
+++ apps/www/registry/magicui/text-animate.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { ElementType, memo } from "react"
-import { AnimatePresence, motion, MotionProps, Variants } from "motion/react"
+import { AnimatePresence, motion, type MotionProps, Variants } from "motion/react"
 
 import { cn } from "@/lib/utils"
 
