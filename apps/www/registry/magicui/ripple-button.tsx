4f10ec39158d04f1e1ba914972cc4a2678e44a92
Vinicius Ventura de Oliveira <42718816+ventura-v@users.noreply.github.com>
2026-03-04 09:04:34 -0300

    fix: add transform scale to ripple effect

--- apps/www/registry/magicui/ripple-button.tsx
+++ apps/www/registry/magicui/ripple-button.tsx
@@ -78,6 +78,7 @@ export const RippleButton = React.forwardRef<
                 top: `${ripple.y}px`,
                 left: `${ripple.x}px`,
                 backgroundColor: rippleColor,
+                transform: `scale(0)`,
                 "--duration": duration,
               } as React.CSSProperties }
             />
