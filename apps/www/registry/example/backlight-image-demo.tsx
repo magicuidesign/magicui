e92c4f91293dab08ac36888caf079099b375f26b
Abdimajid Shire <abdimajid.ms99.com>
2026-03-29 08:08:33 +0300

    fix(backlight-image-demo): make demo component mobile responsive

--- apps/www/registry/example/backlight-image-demo.tsx
+++ apps/www/registry/example/backlight-image-demo.tsx
@@ -2,9 +2,9 @@ import { Backlight } from "@/registry/magicui/backlight"
 
 export default function BacklightImageDemo() {
   return (
-    <Backlight>
+    <Backlight className="w-full">
       <img
-        className="block max-w-lg rounded-xl"
+        className="mx-auto aspect-video w-full max-w-lg rounded-xl"
         src={
           "https://plus.unsplash.com/premium_photo-1672201106204-58e9af7a2888?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
