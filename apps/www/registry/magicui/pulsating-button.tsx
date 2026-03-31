c3e735e82d66cb5dbe63e83373b8079b3ac895e3
Abdimajid Shire <abdimajid.ms99.com>
2026-03-31 09:16:45 +0300

    refactor(pulsating-button): remove unnecessary markup

--- apps/www/registry/magicui/pulsating-button.tsx
+++ apps/www/registry/magicui/pulsating-button.tsx
@@ -25,7 +25,7 @@ export const PulsatingButton = React.forwardRef<
       <button
         ref={ref}
         className={cn(
-          "bg-primary text-primary-foreground relative flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-center",
+          "bg-primary text-primary-foreground animate-pulse cursor-pointer rounded-lg px-4 py-2",
           className
         )}
         style={
@@ -36,8 +36,7 @@ export const PulsatingButton = React.forwardRef<
         }
         {...props}
       >
-        <div className="relative z-10">{children}</div>
-        <div className="absolute top-1/2 left-1/2 size-full -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-lg bg-inherit" />
+        {children}
       </button>
     )
   }
