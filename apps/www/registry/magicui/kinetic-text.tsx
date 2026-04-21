6535f67dd208360437d8bdccd7bac50b1fb4b0fa
Abdimajid Shire <abdimajid.ms99.com>
2026-04-21 14:27:35 +0300

    fix: address pr feedback

--- apps/www/registry/magicui/kinetic-text.tsx
+++ apps/www/registry/magicui/kinetic-text.tsx
@@ -1,9 +1,12 @@
-import { ElementType } from "react"
+import React from "react"
 
-type KineticTextProps = {
+import { cn } from "@/lib/utils"
+
+type As = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span"
+
+type KineticTextProps = React.ComponentProps<As> & {
   text: string
-  as?: ElementType
-  className?: string
+  as?: As
 }
 
 export function KineticText({
@@ -13,17 +16,19 @@ export function KineticText({
 }: KineticTextProps) {
   return (
     <Tag
-      className={["flex flex-wrap font-[300]", className].join(" ")}
-      style={{
-        "--hover-padding": "calc(1em / 12)",
-        "--text-stroke-width": "calc(1em * 125 / 6000)",
-      }}
+      className={cn("flex flex-wrap font-[300]", className)}
+      style={
+        {
+          "--hover-padding": "calc(1em / 12)",
+          "--text-stroke-width": "calc(1em * 125 / 6000)",
+        } as React.CSSProperties
+      }
     >
       {text.split("").map((letter, i) => (
         <span
           key={i}
           aria-hidden="true"
-          className="// letters to the right // letters to the left [will-change:font-weight,-webkit-text-stroke-width,padding] [-webkit-text-stroke-color:transparent] [-webkit-text-stroke-width:var(--text-stroke-width)] [transition:font-weight_0.4s,_-webkit-text-stroke-color_0.4s,_padding_0.4s] hover:[padding-inline:var(--hover-padding)] hover:font-[900] hover:[-webkit-text-stroke-color:currentcolor] hover:[-webkit-text-stroke-width:calc(var(--text-stroke-width)*2)] has-[+span+span:hover]:font-[400] has-[+span:hover]:[padding-inline:var(--hover-padding)] has-[+span:hover]:font-[600] [:hover+&]:[padding-inline:var(--hover-padding)] [:hover+&]:font-[600] [:hover+span+&]:font-[400]"
+          className="[will-change:font-weight,-webkit-text-stroke-width,padding] [-webkit-text-stroke-color:transparent] [-webkit-text-stroke-width:var(--text-stroke-width)] [transition:font-weight_0.4s,_-webkit-text-stroke-color_0.4s,_padding_0.4s] hover:[padding-inline:var(--hover-padding)] hover:font-[900] hover:[-webkit-text-stroke-color:currentcolor] hover:[-webkit-text-stroke-width:calc(var(--text-stroke-width)*2)] has-[+span+span:hover]:font-[400] has-[+span:hover]:[padding-inline:var(--hover-padding)] has-[+span:hover]:font-[600] [:hover+&]:[padding-inline:var(--hover-padding)] [:hover+&]:font-[600] [:hover+span+&]:font-[400]"
         >
           {letter === " " ? "\u00A0" : letter}
         </span>
